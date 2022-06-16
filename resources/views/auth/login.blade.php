cedf554a354263283d43322b4dabae1041e49c14
LT <leetoplayaz@yandex.ru>
2022-06-16 16:46:47 +0300

    fixed auth input color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,12 +25,12 @@
                 <div class="rounded-md shadow-sm">
                     <div>
                         <input value="{{ old("email") }}" aria-label="{{ trans('moonshine::ui.login.email') }}" name="email" type="email" required
-                               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-t-md focus:outline-none focus:shadow-outline-purple focus:border-purple focus:z-10 sm:text-sm sm:leading-5"
+                               class="text-black appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-t-md focus:outline-none focus:shadow-outline-purple focus:border-purple focus:z-10 sm:text-sm sm:leading-5"
                                placeholder="{{ trans('moonshine::ui.login.email') }}">
                     </div>
                     <div class="-mt-px">
                         <input aria-label="{{ trans('moonshine::ui.login.password') }}" name="password" type="password" required
-                               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-purple focus:border-purple focus:z-10 sm:text-sm sm:leading-5"
+                               class="text-black appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-purple focus:border-purple focus:z-10 sm:text-sm sm:leading-5"
                                placeholder="{{ trans('moonshine::ui.login.password') }}">
                     </div>
                 </div>
