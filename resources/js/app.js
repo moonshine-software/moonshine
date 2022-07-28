60c6c6aab454c0f66bd0f6cb8a1330f7d9e08776
Alexander Opekunov <iam@opekunov.com>
2022-07-28 21:03:42 +0300

    migrate to vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
 import mask from '@alpinejs/mask'
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
 Alpine.plugin(persist)
 Alpine.plugin(mask)
