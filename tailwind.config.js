c927460308870f3345b158fb75ee6da2dd9d78df
LT <leetoplayaz@yandex.ru>
2022-07-29 16:47:27 +0300

    fixed js config cs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './resources/views/**/*.blade.php',
-  ],
-  theme: {
-    extend: {
-      gridTemplateRows: {
-        '[auto,auto,1fr]': 'auto auto 1fr',
-      },
-      colors: {
-        black: "#3f3960",
-        purple: "#7665FF",
-        pink: "#E065EF",
-        dark: "#130d52",
-        darkblue: "#302c79",
-        whiteblue: "#f6f4fe"
-      },
+    darkMode: 'class',
+    content: [
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                '[auto,auto,1fr]': 'auto auto 1fr',
+            },
+            colors: {
+                black: "#3f3960",
+                purple: "#7665FF",
+                pink: "#E065EF",
+                dark: "#130d52",
+                darkblue: "#302c79",
+                whiteblue: "#f6f4fe"
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
