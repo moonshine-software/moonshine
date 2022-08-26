0da9f9157c1c920a50d9ef848f09403d9b64783b
Alexander Opekunov <iam@opekunov.com>
2022-08-26 15:50:52 +0300

    remove cors

--- src/Http/Middleware/ConfigureSanctum.php
+++ src/Http/Middleware/ConfigureSanctum.php
@@ -14,10 +14,6 @@ class ConfigureSanctum
         if ($request->routeIs(config('moonshine.prefix').'.*')) {
             config()->set('sanctum.guard', ['moonshine']);
             config()->set('sanctum.stateful', config('moonshine.stateful'));
-
-            config()->set('cors.path', [config('moonshine.prefix').'/*']);
-            config()->set('cors.supports_credentials', true);
-            config()->set('cors.allowed_origins', config('moonshine.frontend_url'));
         }
 
         return $next($request);
