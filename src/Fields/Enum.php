c3c1c10514c5a2169ed7d0ce6dbab666ace33273
LT <leetoplayaz@yandex.ru>
2022-09-18 14:51:56 +0300

    added Enum

---
+++ src/Fields/Enum.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leeto\MoonShine\Fields;
+
+use Illuminate\Database\Eloquent\Model;
+use UnitEnum;
+
+class Enum extends Select
+{
+    public function indexViewValue(Model $item, bool $container = true): mixed
+    {
+        $value = $item->{$this->field()};
+
+        if (isset($this->values()[$value->value])) {
+            return $this->values()[$value->value];
+        }
+
+        return parent::indexViewValue($item, $container);
+    }
+
+    public function attach(string $class): static
+    {
+        /* @var UnitEnum $class ; */
+        $this->options(array_column($class::cases(), 'name', 'value'));
+
+        return $this;
+    }
+
+    public function isSelected(Model $item, string $value): bool
+    {
+        $formValue = $this->formViewValue($item);
+
+        if (!$formValue) {
+            return false;
+        }
+
+        return (string) $formValue->value === $value
+            || (!$formValue->value && (string) $this->getDefault() === $value);
+    }
+}
