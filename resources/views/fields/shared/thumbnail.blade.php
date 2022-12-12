8a5e687e72670c2cfb6933385367d79456d7ad4c
alexvenga <bubnov.alexey@gmail.com>
2022-12-12 14:28:36 +0300

    Update thumbnail.blade.php
    В таблице картинку пережимает как хочет(
    Не очень красиво)
    
    Добавил object-cover
    
    Пересобирать frontend не нужно - класс уже используется и есть в public/vendor/moonshine/css/moonshine.css

--- resources/views/fields/shared/thumbnail.blade.php
+++ resources/views/fields/shared/thumbnail.blade.php
@@ -1,1 +1,1 @@
-<div class="h-10 w-10"><img class="h-10 w-10 rounded-full" src="{{ $value }}" /></div>
+<div class="h-10 w-10"><img class="h-10 w-10 rounded-full object-cover" src="{{ $value }}" /></div>
