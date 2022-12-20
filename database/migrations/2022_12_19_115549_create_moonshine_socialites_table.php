ccddaa19168bbaacde16b4dc1edab887da38a970
alexvenga <bubnov.alexey@gmail.com>
2022-12-20 06:28:51 +0300

    Update 2022_12_19_115549_create_moonshine_socialites_table.php
    S

--- database/migrations/2022_12_19_115549_create_moonshine_socialites_table.php
+++ database/migrations/2022_12_19_115549_create_moonshine_socialites_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('moonshine_socialite');
+        Schema::dropIfExists('moonshine_socialites');
     }
 };
