84baba27d7491655f8aaf2e84060f7425af19916
lee-to <lee-to@users.noreply.github.com>
2022-12-30 15:37:09 +0000

    Fix styling

--- src/Decorations/Decoration.php
+++ src/Decorations/Decoration.php
@@ -47,7 +47,7 @@ abstract class Decoration implements HtmlViewable
         foreach ($fields as $field) {
             if ($field instanceof Field) {
                 $this->fields[] = $field->setParents();
-            } elseif(!$field instanceof Tab) {
+            } elseif (! $field instanceof Tab) {
                 $this->fields[] = $field;
             }
         }
