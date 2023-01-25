487635d66666abea5943d4ce59a84f17dd9ce37a
alexvenga <bubnov.alexey@gmail.com>
2023-01-25 14:58:53 +0300

    Add classes

--- resources/views/shared/icons/heroicons/ellipsis-horizontal.blade.php
+++ resources/views/shared/icons/heroicons/ellipsis-horizontal.blade.php
@@ -1,3 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+     class="fill-current w-{{ $size ?? 5 }} h-{{ $size ?? 5 }} {{ $class ?? '' }} {{ isset($color) ? "text-$color" : '' }}">
   <path fill-rule="evenodd" d="M4.5 12a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm6 0a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm6 0a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" clip-rule="evenodd"/>
 </svg>
--- resources/views/shared/icons/heroicons/minus-circle.blade.php
+++ resources/views/shared/icons/heroicons/minus-circle.blade.php
@@ -1,3 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+     class="fill-current w-{{ $size ?? 5 }} h-{{ $size ?? 5 }} {{ $class ?? '' }} {{ isset($color) ? "text-$color" : '' }}">
   <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z" clip-rule="evenodd"/>
 </svg>
