dfaf0c5304dc1dff3ea1ea4045d971e68edb2426
alexvenga <bubnov.alexey@gmail.com>
2023-01-27 13:15:50 +0300

    Small NoInput fix (на всякий случай, если вдруг чтото изменится в базовом field)

--- src/Fields/NoInput.php
+++ src/Fields/NoInput.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace Leeto\MoonShine\Fields;
 
+use Illuminate\Database\Eloquent\Model;
+
 class NoInput extends Field
 {
     public static string $view = 'moonshine::fields.no-input';
+
+    public function save(Model $item): Model
+    {
+        return $item;
+    }
+
 }
