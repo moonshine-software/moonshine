f567ffddd663f9f394acae8e7988e3607c70b511
Alexander Nikushkin <nikushkin@mail.ru>
2023-02-09 11:36:51 +0500

    Decorator block disabled margin-collapsing

--- resources/views/decorations/block.blade.php
+++ resources/views/decorations/block.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white dark:bg-darkblue shadow-md rounded-lg mb-4">
+<div class="bg-white dark:bg-darkblue shadow-md rounded-lg mb-4 grid">
     <x-moonshine::resource-renderable
         :components="$decoration->fields()"
         :item="$item"
