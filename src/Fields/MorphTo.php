69ad4c9f45ec0c6dd750da99244d3927609ce98e
LT <leetoplayaz@yandex.ru>
2023-02-15 11:16:13 +0300

    new morph fields

---
+++ src/Fields/MorphTo.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leeto\MoonShine\Fields;
+
+class MorphTo extends BelongsTo
+{
+}
---
+++ src/Fields/MorphToMany.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leeto\MoonShine\Fields;
+
+class MorphToMany extends BelongsToMany
+{
+}
