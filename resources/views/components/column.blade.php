7faaa254c5794af0823f10f1ed8cc07cd59d0a47
second1996 <vital10012@gmail.com>
2023-03-19 15:12:26 +0200

    fix(new design): Fix column spaces

--- resources/views/components/column.blade.php
+++ resources/views/components/column.blade.php
@@ -2,6 +2,6 @@
     'adaptiveColSpan' => 12,
     'colSpan' => 12,
 ])
-<div class="col-span-{{ $adaptiveColSpan }} xl:col-span-{{ $colSpan }}">
+<div class="col-span-{{ $adaptiveColSpan }} xl:col-span-{{ $colSpan }} space-y-6">
     {{ $slot }}
 </div>
