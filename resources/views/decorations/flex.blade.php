d7e68a7688c7a9f93cb30345c9ee6a305c923cfd
second1996 <vital10012@gmail.com>
2023-03-19 12:52:56 +0200

    fix(new-design): Restored flex wrapper in decorations/flex

--- resources/views/decorations/flex.blade.php
+++ resources/views/decorations/flex.blade.php
@@ -1,5 +1,12 @@
-<x-moonshine::resource-renderable
-    :components="$decoration->fields()"
-    :item="$item"
-    :resource="$resource"
-/>
+<div class="sm:flex
+    @if(!$decoration->isWithoutSpace()) gap-4 @endif
+    items-{{ $decoration->getItemsAlign() }}
+    justify-{{ $decoration->getJustifyAlign() }}"
+>
+    <x-moonshine::resource-renderable
+        :components="$decoration->fields()"
+        :item="$item"
+        :resource="$resource"
+        :container="true"
+    />
+</div>
