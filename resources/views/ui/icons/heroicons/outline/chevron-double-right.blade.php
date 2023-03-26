18bd5b7262043f2ddc2cc95f1eb1a6e9932e2765
alexvenga <bubnov.alexey@gmail.com>
2023-03-26 16:45:04 +0300

    Add files via upload
    Icons

---
+++ resources/views/ui/icons/heroicons/outline/chevron-double-right.blade.php
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"
+	class="text-current w-{{ $size ?? 5 }} h-{{ $size ?? 5 }} {{ $class ?? '' }} {{ isset($color) ? "text-$color" : '' }}">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"/>
+</svg>
