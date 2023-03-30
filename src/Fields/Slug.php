7c2cf807f99acfd3beefe29a6fee1bbca1a75e4c
SiTiS <hash14@rambler.ru>
2023-03-30 16:10:13 +0700

    Add slug field

--- src/Fields/Slug.php
+++ src/Fields/Slug.php
@@ -6,28 +6,28 @@ namespace Leeto\MoonShine\Fields;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-use Leeto\MoonShine\Traits\Fields\WithMask;
 
 class Slug extends Field
 {
-    use WithMask;
-
     public static string $view = 'moonshine::fields.input';
 
     public static string $type = 'text';
 
     protected string $from;
+
     protected string $separator = '-';
 
     public function from(string $from): static
     {
         $this->from = $from;
+
         return $this;
     }
 
     public function separator(string $separator): static
     {
         $this->separator = $separator;
+
         return $this;
     }
 
