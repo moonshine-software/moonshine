4ad00f99a2ccdf2c39a5958090adad79a7fe1b43
alexvenga <bubnov.alexey@gmail.com>
2023-03-31 14:58:43 +0300

    Footer nav & copy

--- resources/views/layouts/shared/footer.blade.php
+++ resources/views/layouts/shared/footer.blade.php
@@ -1,11 +1,13 @@
 <!-- Footer -->
 <footer class="layout-footer">
     <div class="flex flex-col flex-wrap items-center justify-between gap-y-4 gap-x-8 md:flex-row">
-        <div class="text-center text-2xs text-slate-500 md:text-left">&copy; 2021-{{ date('Y') }}. Made with ❤️ by <a href="https://cutcode.dev/" class="font-semibold text-purple hover:text-pink" target="_blank">CutCode</a></div>
+        <div class="text-center text-2xs text-slate-500 md:text-left">
+            &copy; 2021-{{ date('Y') }}. {!! config('moonshine.footer.copyright') !!}
+        </div>
         <nav class="flex flex-wrap justify-center gap-x-4 gap-y-2 md:justify-start lg:gap-x-6">
-            <a href="https://github.com/lee-to/moonshine/blob/1.x/LICENSE.md" class="text-2xs text-slate-500 hover:text-purple" target="_blank">License</a>
-            <a href="https://moonshine.cutcode.dev/" class="text-2xs text-slate-500 hover:text-purple" target="_blank">Documentation</a>
-            <a href="https://github.com/lee-to/moonshine" class="text-2xs text-slate-500 hover:text-purple" target="_blank">GitHub</a>
+            @foreach(config('moonshine.footer.nav') as $url=>$text)
+                <a href="{{ $url }}" class="text-2xs text-slate-500 hover:text-purple" target="_blank">{!! $text !!}</a>
+            @endforeach
         </nav>
     </div>
 </footer>
