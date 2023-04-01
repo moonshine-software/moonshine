e969d370f94fd0696314c74e5577b46f7a341b65
SiTiS <hash14@rambler.ru>
2023-04-01 18:02:10 +0700

    Add unique in slug field

--- src/Fields/Slug.php
+++ src/Fields/Slug.php
@@ -1,11 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
-namespace Leeto\MoonShine\Fields;
+namespace App\MoonShine\Fields;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Leeto\MoonShine\Fields\Field;
 
 class Slug extends Field
 {
@@ -16,6 +16,8 @@ class Slug extends Field
     protected string $from;
 
     protected string $separator = '-';
+
+    protected bool $unique = false;
 
     public function from(string $from): static
     {
@@ -31,16 +33,49 @@ class Slug extends Field
         return $this;
     }
 
+    public function unique(): static
+    {
+        $this->unique = true;
+
+        return $this;
+    }
+
     public function save(Model $item): Model
     {
         if (! $this->canSave) {
             return $item;
         }
 
-        $item->{$this->field()} = $this->requestValue() !== false
-            ? $this->requestValue()
-            : (isset($this->from) ? Str::slug($item->{$this->from}, $this->separator) : '');
+        $item->{$this->field()} = $this->requestValue() !== false ? $this->requestValue() : $this->generateSlug($item->{$this->from});
+
+        if ($this->unique)
+        {
+            $item->{$this->field()} = $this->makeSlugUnique($item);
+        }
 
         return $item;
     }
+
+    private function generateSlug(string $value): string
+    {
+        return Str::slug($value, $this->separator);
+    }
+
+    protected function checkUnique(Model $item, string $slug): bool
+    {
+        return !DB::table($item->getTable())->whereNot('id', $item->id)->where($this->field(), $slug)->first();
+    }
+
+    protected function makeSlugUnique(Model $item): string
+    {
+        $slug = $item->{$this->field()};
+        $i = 1;
+
+        while (!$this->checkUnique($item, $slug)) {
+            $slug = $item->{$this->field()}.$this->separator.$i++;
+        }
+
+        return $slug;
+    }
+
 }
