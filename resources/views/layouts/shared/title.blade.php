4dcd42037724fbd35ba2239e3795a5ec5186ff5d
LT <leetoplayaz@yandex.ru>
2023-04-04 18:33:40 +0300

    fix(new-design): New design fixes
    Fixed sub title

--- resources/views/layouts/shared/title.blade.php
+++ resources/views/layouts/shared/title.blade.php
@@ -1,7 +1,9 @@
 @section('title', $title)
 
-<h1 class="mb-6 truncate text-md font-medium">{{ $title }}</h1>
+<h1 class="{{ isset($subTitle) && $subTitle ?: 'mb-6' }} truncate text-md font-medium">
+    {{ $title }}
+</h1>
 
 @if($subTitle ?? false)
-    <div class="pt-2">{!! $subTitle !!}</div>
+    <div class="pt-2 mb-6">{!! $subTitle !!}</div>
 @endif
