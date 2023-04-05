ae9bd6126652a7f3fa308b44be3f9cfdc1cd30fc
DissNik <nikushkin@mail.ru>
2023-04-05 20:23:11 +0500

    remove cdn

--- src/Fields/WYSIWYG.php
+++ src/Fields/WYSIWYG.php
@@ -9,7 +9,7 @@ class WYSIWYG extends Field
     protected static string $view = 'moonshine::fields.wysiwyg';
 
     protected array $assets = [
-        'vendor/moonshine/js/trix/trix.js',
-        'vendor/moonshine/css/trix/trix.css',
+        'vendor/moonshine/libs/trix/trix.js',
+        'vendor/moonshine/libs/trix/trix.css',
     ];
 }
