eca71f77e01344ebfa734e1599bb84589c2eaca9
alexvenga <bubnov.alexey@gmail.com>
2023-04-06 17:50:23 +0300

    fix(new-design): NoInput
    Add ->link(string|Closure $link = '#', bool $blank = false) to NoInput

--- resources/views/ui/url.blade.php
+++ resources/views/ui/url.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $href ?? null }}" class="inline-flex items-center gap-1" @if(!empty($blank)) target="_blank" @endif>
+<a href="{{ $href ?? '#' }}" class="inline-flex items-center gap-1" @if(!empty($blank)) target="_blank" @endif>
     <x-moonshine::icon icon="heroicons.link" />
 
     {{ $value ?? null }}
