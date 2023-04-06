938a237e714877e2cae4b1d93d459f24b3c547b8
alexvenga <bubnov.alexey@gmail.com>
2023-04-06 12:29:53 +0300

    Update no-input-boolean.blade.php
    fix(new-design): NoInputBoolean
    
    Change NoInputBoolean view from badge to dot

--- resources/views/fields/no-input-boolean.blade.php
+++ resources/views/fields/no-input-boolean.blade.php
@@ -1,13 +1,9 @@
 <div>
     @if((isset($value) && !is_null($value)) || (isset($element) && !is_null($element->formViewValue($item))))
         @if((isset($value) && $value) || (isset($element) && $element->formViewValue($item)))
-            <x-moonshine::badge color='green'>
-                <x-moonshine::icon icon='heroicons.outline.check'/>
-            </x-moonshine::badge>
+            <div class="mx-auto h-2 w-2 rounded-full bg-green-500"></div>
         @else
-            <x-moonshine::badge color='red'>
-                <x-moonshine::icon icon='heroicons.outline.x-mark'/>
-            </x-moonshine::badge>
+            <div class="mx-auto h-2 w-2 rounded-full bg-red-500"></div>
         @endif
     @endif
 </div>
