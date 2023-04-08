b84bf23227937ba9ed5d3c37853d8c1228124cf0
LT <leetoplayaz@yandex.ru>
2023-04-09 00:19:59 +0300

    fix(fields): Checkbox
    view fixed

--- resources/views/fields/checkbox.blade.php
+++ resources/views/fields/checkbox.blade.php
@@ -1,7 +1,24 @@
 <div>
-    @include('moonshine::fields.' . ($element->isGroup() ? 'multi-checkbox' : 'input'), [
-        'element' => $element,
-        'item' => $item,
-        'resource' => $resource
-    ])
+    @if($element->isGroup())
+        @include('moonshine::fields.multi-checkbox', [
+            'element' => $element,
+            'item' => $item,
+            'resource' => $resource
+        ])
+    @else
+        <x-moonshine::form.input
+            type="hidden"
+            name="{{ $element->name() }}"
+            value="{{ $element->getOffValue() }}"
+        />
+
+        <x-moonshine::form.input
+            :attributes="$element->attributes()->merge([
+                'id' => $element->id(),
+                'name' => $element->name(),
+                'value' => $element->getOnValue()
+            ])"
+            @class(['form-invalid' => $errors->has($element->name())])
+        />
+    @endif
 </div>
