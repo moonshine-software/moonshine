608c072f24f9368e5a51e7ee4067ae423484c9fe
LT <leetoplayaz@yandex.ru>
2023-04-19 18:54:57 +0300

    feat(decorations): Divider
    New decoration divider

---
+++ src/Decorations/Divider.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\Decorations;
+
+class Divider extends Decoration
+{
+    protected static string $view = 'moonshine::decorations.divider';
+}
