9b75ce955e10c6173687ff17ed3a0f8597624366
DissNik <nikushkin@mail.ru>
2023-05-05 09:25:34 +0500

    fix(components): default alert type "default"

--- resources/views/layouts/shared/flash.blade.php
+++ resources/views/layouts/shared/flash.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('alert'))
-    <x-moonshine::alert :removable="true">
+    <x-moonshine::alert :removable="true" type="info">
         {{ session()->get('alert') }}
     </x-moonshine::alert>
 @endif
