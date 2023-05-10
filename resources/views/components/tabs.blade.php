176fcd8de637cdc1b02932ef81fc521ba98c940f
DissNik <nikushkin@mail.ru>
2023-05-10 21:59:19 +0500

    fix(components): tabs

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,18 +1,17 @@
 @props([
-    'id',
     'tabs',
     'contents'
 ])
 @if($tabs)
     <!-- Tabs -->
-    <div class="tabs mb-4" x-data="{ {{ $id }}: '{{ array_key_first($tabs) }}'}">
+    <div class="tabs mb-4" x-data="{ activeTab: '{{ array_key_first($tabs) }}'}">
         <!-- Tabs Buttons -->
         <ul class="tabs-list">
             @foreach($tabs as $tabId => $tabContent)
                 <li class="tabs-item">
                     <button
-                        @click.prevent="{{ $id }} = '{{ $tabId }}'"
-                        :class="{ '_is-active': {{ $id }} === '{{ $tabId}}' }"
+                        @click.prevent="activeTab = '{{ $tabId }}'"
+                        :class="{ '_is-active': activeTab === '{{ $tabId}}' }"
                         class="tabs-button"
                         type="button"
                     >
@@ -26,7 +25,7 @@
         <!-- Tabs content -->
         <div class="tabs-content">
             @foreach($contents as $tabId => $tabContent)
-                <div x-show="{{ $id }} === '{{ $tabId }}'" class="tab-panel" style="display: none">
+                <div x-show="activeTab === '{{ $tabId }}'" class="tab-panel" style="display: none">
                     <div class="tabs-body">
                         {!! $tabContent !!}
                     </div>
