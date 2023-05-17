ddf247721cfd65f674231c4cb7d0ce5c33792de3
LT <leetoplayaz@yandex.ru>
2023-05-17 21:21:25 +0300

    feat(menu): Divider
    New decoration item

--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -1,14 +1,20 @@
 @props([
     'item'
 ])
-<li class="menu-inner-item {{ $item->isActive() ? '_is-active' : '' }}">
-    <a href="{{ $item->url() }}" class="menu-inner-link" x-data="navTooltip" @mouseenter="toggleTooltip()">
-        {!! $item->getIcon(6) !!}
+@if($item instanceof MoonShine\Menu\MenuDivider)
+    <li class="menu-inner-divider">
+        {!! $item->label() ? "<span>{$item->label()}</span>" : '' !!}
+    </li>
+@else
+    <li class="menu-inner-item {{ $item->isActive() ? '_is-active' : '' }}">
+        <a href="{{ $item->url() }}" class="menu-inner-link" x-data="navTooltip" @mouseenter="toggleTooltip()">
+            {!! $item->getIcon(6) !!}
 
-        <span class="menu-inner-text">{{ $item->label() }}</span>
+            <span class="menu-inner-text">{{ $item->label() }}</span>
 
-        @if($item->hasBadge())
-            <span class="menu-inner-counter">{{ $item->getBadge() }}</span>
-        @endif
-    </a>
-</li>
+            @if($item->hasBadge())
+                <span class="menu-inner-counter">{{ $item->getBadge() }}</span>
+            @endif
+        </a>
+    </li>
+@endif
