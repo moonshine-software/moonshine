c776a31a873e6f4e3e5b67ff2a6c900982fad85b
Dzianis Kotau <me@dzianiskotau.com>
2023-05-24 01:17:02 +0400

    fix(change-log): Log actions if requester is MoonshineUser only

--- src/Traits/Models/HasMoonShineChangeLog.php
+++ src/Traits/Models/HasMoonShineChangeLog.php
@@ -7,6 +7,7 @@ namespace MoonShine\Traits\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use MoonShine\Models\MoonshineChangeLog;
+use MoonShine\Models\MoonshineUser;
 use MoonShine\MoonShineAuth;
 
 trait HasMoonShineChangeLog
@@ -24,7 +25,7 @@ trait HasMoonShineChangeLog
 
     public function createLog(): void
     {
-        if (MoonShineAuth::guard()->check()) {
+        if (MoonShineAuth::guard()->check() && request()->user() instanceof MoonshineUser) {
             $this->changeLogs()->create([
                 'moonshine_user_id' => MoonShineAuth::guard()->id(),
                 'states_before' => $this->getOriginal(),
