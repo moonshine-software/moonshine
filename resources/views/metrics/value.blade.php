3c2fd727c2c369622180feb27fb6f6f9239c32a8
LT <leetoplayaz@yandex.ru>
2023-05-23 20:57:43 +0300

    feat(components): Progress bar
    Progress bar and use it in value metric

--- resources/views/metrics/value.blade.php
+++ resources/views/metrics/value.blade.php
@@ -1,6 +1,6 @@
 <x-moonshine::column
     :colSpan="$element->columnSpanValue()"
-    :adaptiveColSpan="$element->adaptiveColumnSpanValue()"
+    :adaptiveColSpan="$element->adaptiveColumnSpanValue()" xmlns:x-moonshine="http://www.w3.org/1999/html"
 >
     <x-moonshine::box
         class="box-shadow zoom-in h-full p-0"
@@ -8,13 +8,17 @@
         <div class="report-card">
             <div class="report-card-heading">
                 {!! $element->getIcon(6, 'pink') !!}
-
-                @if($element->isProgress())
-                    <div class="report-card-indicator bg-green-500">
-                        {{ $element->valueResult() }}%
-                    </div>
-                @endif
             </div>
+
+            @if($element->isProgress())
+                <x-moonshine::progress-bar
+                    color="purple"
+                    :radial="false"
+                    :value="$element->valueResult()"
+                >
+                    {{ $element->valueResult() }}%
+                </x-moonshine::progress-bar>
+            @endif
 
             <div class="report-card-body">
                 <div class="report-card-value">{{ $element->simpleValue() }}</div>
