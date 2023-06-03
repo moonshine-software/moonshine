512909d11f7df82e242bb667d58927cecc3118b9
LT <leetoplayaz@yandex.ru>
2023-06-04 00:12:15 +0300

    feat(components): IndexComponent/Logos
    IndexComponent and logo small

--- resources/views/layouts/shared/logo.blade.php
+++ resources/views/layouts/shared/logo.blade.php
@@ -1,19 +1,12 @@
 <a href="{{ route('moonshine.index') }}" class="block" rel="home">
-    @if(config('moonshine.logo'))
-        <img class="h-14 xl:block"
-             src="{{ config('moonshine.logo') }}"
-             alt="{{ config('moonshine.title') }}"
-        />
-    @else
-        <img src="{{ asset('vendor/moonshine/logo.svg') }}"
+        <img src="{{ config('moonshine.logo') ?: asset('vendor/moonshine/logo.svg') }}"
              class="hidden h-14 xl:block"
              :class="minimizedMenu && '!hidden'"
              alt="{{ config('moonshine.title') }}"
         />
-        <img src="{{ asset('vendor/moonshine/logo-small.svg') }}"
+        <img src="{{ config('moonshine.logo_small') ?: asset('vendor/moonshine/logo-small.svg') }}"
              class="block h-8 lg:h-10 xl:hidden"
              :class="minimizedMenu && '!block'"
              alt="{{ config('moonshine.title') }}"
         />
-    @endif
 </a>
