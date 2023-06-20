20282fb20b79bdc06ea65ba567f54c08c592cc54
DissNik <nikushkin@mail.ru>
2023-06-20 17:53:14 +0500

    fix(components): closing of other modals
    fixed bug of closing all modal windows when outside is clicked

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -18,12 +18,13 @@
             class="modal"
             aria-modal="true"
             role="dialog"
+            @click.self="open=false"
         >
             <div class="modal-dialog @if($wide) modal-dialog-xl @elseif($auto) modal-dialog-auto @endif" x-bind="dismissModal">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">{{ $title ?? '' }}</h5>
-                        <button type="button" class="btn btn-close" @click.prevent="toggleModal" aria-label="Close">
+                        <button type="button" class="btn btn-close" @click.stop="open=false" aria-label="Close">
                             <x-moonshine::icon
                                 icon="heroicons.x-mark"
                                 size="6"
