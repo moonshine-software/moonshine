6682275ed78b93d547c4f718786deae88e995d1f
YouMixx <tyoumixx@gmail.com>
2023-07-17 01:31:16 +0400

    fix(fields): showWhen

--- resources/js/alpine/showWhenFunctions.js
+++ resources/js/alpine/showWhenFunctions.js
@@ -40,7 +40,7 @@ export function showWhenVisibilityChange(fieldName, inputs, field) {
     return
   }
 
-  const fieldContainer = inputElement.closest('.form-group')
+  const fieldContainer = inputElement.closest('#wrapper_')
 
   let validateShow = false
 
