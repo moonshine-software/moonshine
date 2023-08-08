1bff647cea6953fad9a321a8500753bf24eaed38
lee-to <lee-to@users.noreply.github.com>
2023-08-08 12:44:04 +0000

    [rector] Rector fixes

--- src/Components/MoonshineComponent.php
+++ src/Components/MoonshineComponent.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace MoonShine\Components;
 
+use Stringable;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
 use Illuminate\View\Component;
 use MoonShine\Traits\Makeable;
 use Throwable;
 
-abstract class MoonshineComponent extends Component
+abstract class MoonshineComponent extends Component implements Stringable
 {
     use Makeable;
     use Conditionable;
