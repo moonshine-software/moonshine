d28acfb85de2c5f29000475ddc4226a2ced3baa3
levchenko-ivan <ivan97111@yandex.ru>
2023-08-09 18:46:04 +0300

    fix(page): IndexPage
    IndexPage return standart view

--- src/Pages/Crud/IndexPage.php
+++ src/Pages/Crud/IndexPage.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace MoonShine\Pages\Crud;
 
-use Illuminate\View\ComponentAttributeBag;
 use MoonShine\ActionButtons\ActionButton;
 use MoonShine\Actions\ExportAction;
 use MoonShine\Actions\FiltersAction;
 use MoonShine\Components\ActionGroup;
+use MoonShine\Components\TableBuilder;
 use MoonShine\Decorations\Column;
 use MoonShine\Decorations\Flex;
 use MoonShine\Decorations\Grid;
@@ -30,72 +30,31 @@ class IndexPage extends Page
         return [
             Grid::make([
                 Column::make([
-                    ValueMetric::make('Metric 1')->value(200),
-                ])->columnSpan(6),
-
-                Column::make([
-                    ValueMetric::make('Metric 2')->value(300),
-                ])->columnSpan(6),
-
-                Column::make([
                     Flex::make([
-                        ActionButton::make('Добавить', to_page($this->getResource(), FormPage::class))
+                        ActionButton::make(__('moonshine::ui.create'), to_page($this->getResource(), FormPage::class))
                             ->customAttributes(['class' => 'btn btn-primary'])
                             ->icon('heroicons.outline.plus'),
-
-                        Modal::make('Добавить No Async')
-                            ->content('Title', 'Hello world')
-                            ->customAttributes(['class' => 'btn btn-primary'])
-                            ->icon('heroicons.outline.plus'),
-
-                        Modal::make('Добавить Async')
-                            ->async($this->route())
-                            ->wide()
-                            ->content('Title', 'Hello world')
-                            ->customAttributes(['class' => 'btn btn-primary'])
-                            ->icon('heroicons.outline.plus'),
-
-                        Offcanvas::make('Offcanvas')
-                            ->content('Title', 'Hello world')
-                            ->customAttributes(['class' => 'btn btn-primary'])
-                            ->icon('heroicons.outline.plus'),
-
-                        ActionGroup::make([
-                            ExportAction::make('Export')
-                                ->setResource($this->getResource())
-                                ->showInDropdown(),
-                        ]),
                     ])->justifyAlign('start'),
 
                     ActionGroup::make([
-                        FiltersAction::make('Filters')
+                        FiltersAction::make(__('moonshine::ui.filters'))
                             ->filters([
-                                Text::make('Test'),
+                                Text::make('Отель', 'name'),
+                                Text::make('Домен', 'domain'),
                             ])
                             ->setResource($this->getResource())
                             ->showInLine(),
                     ]),
-
                 ])->customAttributes([
                     'class' => 'flex flex-wrap items-center justify-between gap-2 sm:flex-nowrap',
                 ]),
             ]),
-            table()->fields($this->getResource()->getIndexFields()->toArray())
+
+            TableBuilder::make()
+                ->fields($this->getResource()->getIndexFields()->toArray())
                 ->cast($this->getResource()->getModelCast())
                 ->items($items->items())
                 ->paginator($items)
-                ->trAttributes(fn ($data, int $index, ComponentAttributeBag $attributes): ComponentAttributeBag => $attributes->when(
-                    $index === 0,
-                    fn (ComponentAttributeBag $attr): ComponentAttributeBag => $attr->merge([
-                        'class' => 'bgc-purple',
-                    ])
-                ))
-                ->tdAttributes(fn ($data, int $cell, int $index, ComponentAttributeBag $attributes): ComponentAttributeBag => $attributes->when(
-                    $index === 1 && $cell === 0,
-                    fn (ComponentAttributeBag $attr): ComponentAttributeBag => $attr->merge([
-                        'class' => 'bgc-red',
-                    ])
-                ))
                 ->buttons([
 
                     ActionButton::make(
@@ -137,12 +96,11 @@ class IndexPage extends Page
                                 $action->url(),
                                 fields: [
                                     Hidden::make('_method')->setValue('DELETE'),
-                                    TextBlock::make('', __('moonshine::ui.confirm_delete')),
+                                    TextBlock::make('', __('moonshine::ui.confirm_message')),
                                 ]
                             )->submit(__('moonshine::ui.delete'), ['class' => 'btn-pink'])
                         )
                         ->showInLine(),
-
 
                     ActionButton::make(
                         '',
@@ -162,12 +120,11 @@ class IndexPage extends Page
                                     Hidden::make('ids')->customAttributes([
                                         'class' => 'actionsCheckedIds',
                                     ]),
-                                    Heading::make(__('moonshine::ui.confirm_delete')),
+                                    Heading::make(__('moonshine::ui.confirm_message')),
                                 ])
                                 ->submit('Delete', ['class' => 'btn-pink'])
                         )
                         ->showInLine(),
-
                 ]),
         ];
     }
