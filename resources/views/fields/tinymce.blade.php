a7300c9eb8691e27ee6028336b94b4e4e336b8a6
DissNik <nikushkin@mail.ru>
2023-08-24 11:26:18 +0500

    feat(fields): menubar config for TinyMce
    issue #439

--- resources/views/fields/tinymce.blade.php
+++ resources/views/fields/tinymce.blade.php
@@ -7,6 +7,7 @@
         x-data="tinymce()"
         :data-language="!empty($element->locale) ? $element->locale : app()->getLocale()"
         :data-plugins="trim($element->plugins . ' ' . $element->addedPlugins)"
+        :data-menubar="trim($element->menubar)"
         :data-toolbar="trim($element->toolbar . ' ' . $element->addedToolbar)"
         :data-tinycomments_mode="!empty($element->commentAuthor) ? 'embedded' : null"
         :data-tinycomments_author="!empty($element->commentAuthor) ? $element->commentAuthor : null"
