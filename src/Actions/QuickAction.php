b8e323b45d422911dd83b743d2c8ba4880880d5f
alexvenga <bubnov.alexey@gmail.com>
2023-09-26 11:58:00 +0300

    feat(old-version): QuickAction
    QuickAction in resource

---
+++ src/Actions/QuickAction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\Actions;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+
+/**
+ * @method static static make(string $label = '', Closure $callback = null)
+ */
+class QuickAction extends Action
+{
+
+    protected Closure $callback;
+
+    public function __construct(string $label = '', Closure $callback = null)
+    {
+
+        if (is_null($callback)) {
+            throw new \InvalidArgumentException('Invalid callback');
+        }
+
+        $this->callback = $callback;
+
+        parent::__construct($label);
+    }
+
+    protected ?string $icon = 'heroicons.outline.bolt';
+
+    public function handle(): RedirectResponse
+    {
+        call_user_func($this->callback);
+
+        return back();
+    }
+
+}
