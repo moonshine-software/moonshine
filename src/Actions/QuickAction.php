f4d7d9872bc28412fae16c02d1af2f28f6956d67
alexvenga <bubnov.alexey@gmail.com>
2023-09-26 12:08:36 +0300

    feat(old-version): QuickAction
    QuickAction in resource

--- src/Actions/QuickAction.php
+++ src/Actions/QuickAction.php
@@ -6,32 +6,41 @@ namespace MoonShine\Actions;
 
 use Closure;
 use Illuminate\Http\RedirectResponse;
+use MoonShine\MoonShineUI;
+use MoonShine\Traits\WithConfirmation;
 
 /**
- * @method static static make(string $label = '', Closure $callback = null)
+ * @method static static make(string $label, Closure $callback, string $message = 'Done')
  */
 class QuickAction extends Action
 {
 
-    protected Closure $callback;
+    use WithConfirmation;
 
-    public function __construct(string $label = '', Closure $callback = null)
+    public function __construct(
+        string $label,
+        protected Closure $callback,
+        protected string $message = 'Done')
     {
-
-        if (is_null($callback)) {
-            throw new \InvalidArgumentException('Invalid callback');
-        }
-
-        $this->callback = $callback;
-
         parent::__construct($label);
     }
 
     protected ?string $icon = 'heroicons.outline.bolt';
 
+    public function message(): string
+    {
+        return $this->message;
+    }
+
     public function handle(): RedirectResponse
     {
+
         call_user_func($this->callback);
+
+        MoonShineUI::toast(
+            $this->message,
+            'info'
+        );
 
         return back();
     }
