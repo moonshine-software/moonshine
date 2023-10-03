b39406a7a0afd8b75769baac9ad2c545a49b08f3
levchenko-ivan <ivan97111@yandex.ru>
2023-10-03 10:50:00 +0300

    test(pages):  Custtom pages test
    Testing custom pages with types

---
+++ tests/Fixtures/Pages/Custom/CustomPageForm.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\Tests\Fixtures\Pages\Custom;
+
+use MoonShine\Decorations\Block;
+use MoonShine\Enums\PageType;
+use MoonShine\Fields\Preview;
+use MoonShine\Pages\Page;
+
+class CustomPageForm extends Page
+{
+    protected ?PageType $pageType = PageType::FORM;
+
+    public function components(): array
+    {
+        return [
+            Block::make([
+                Preview::make('CustomPageForm', formatted: fn() => 'CustomPageForm')
+            ]),
+        ];
+    }
+}
