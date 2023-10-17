68b099b2ea49295dfa6b0483d61498232e1fd9b2
KUANYSH OSMANOV <developerandroidkz@gmail.com>
2023-10-17 21:09:50 +0500

    feat: dir added when created page

--- src/Commands/MakePageCommand.php
+++ src/Commands/MakePageCommand.php
@@ -11,6 +11,7 @@ use function Laravel\Prompts\outro;
 use function Laravel\Prompts\text;
 
 use MoonShine\MoonShine;
+use Illuminate\Support\Collection;
 
 class MakePageCommand extends MoonShineCommand
 {
@@ -30,6 +31,17 @@ class MakePageCommand extends MoonShineCommand
             'Class name',
             required: true
         );
+
+        if(str($className)->contains('/'))
+            $dir = str($className)
+                ->explode('/')
+                ->tap(function (Collection $data) use (&$className): void {
+                    $className = $data->last();
+                })
+                ->slice(0, -1)
+                ->values()
+                ->prepend('Pages')
+                ->implode('/');
 
         $page = $this->getDirectory() . "/$dir/$className.php";
 
