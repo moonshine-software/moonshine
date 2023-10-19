a55ac5b4255561f90bf4af77fbd71d5e028602bc
levchenko-ivan <ivan97111@yandex.ru>
2023-10-19 16:20:40 +0300

    fix(components): QueryTag async Fix
    new disableQueryTags event

--- resources/js/alpine/queryTag.js
+++ resources/js/alpine/queryTag.js
@@ -2,18 +2,21 @@ export default () => ({
   dispatchAsyncEvent(data) {
     if (this.$root.classList.contains('active-query-tag')) {
       this.$dispatch('async-table', {queryTag: 'query-tag=null'})
-      this.$root.classList.remove('btn-primary')
-      this.$root.classList.remove('active-query-tag')
+      this.disableQueryTags()
       return
     }
 
     this.$dispatch('async-table', {queryTag: 'query-tag=' + data})
 
-    document.querySelectorAll('.query-tag-button').forEach(function (element) {
-      element.classList.remove('btn-primary')
-    })
+    this.disableQueryTags()
 
     this.$root.classList.add('btn-primary')
     this.$root.classList.add('active-query-tag')
   },
+  disableQueryTags() {
+    document.querySelectorAll('.query-tag-button').forEach(function (element) {
+      element.classList.remove('btn-primary')
+      element.classList.remove('active-query-tag')
+    })
+  }
 })
