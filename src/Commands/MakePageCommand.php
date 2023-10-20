e41bd4360205f07b3c4563b6e6c887ba6eb539ea
KUANYSH OSMANOV <developerandroidkz@gmail.com>
2023-10-20 15:00:33 +0500

    fix: ignore --dir option

--- src/Commands/MakePageCommand.php
+++ src/Commands/MakePageCommand.php
@@ -11,7 +11,6 @@ use function Laravel\Prompts\outro;
 use function Laravel\Prompts\text;
 
 use MoonShine\MoonShine;
-use Illuminate\Support\Collection;
 
 class MakePageCommand extends MoonShineCommand
 {
@@ -32,16 +31,11 @@ class MakePageCommand extends MoonShineCommand
             required: true
         );
 
-        if(str($className)->contains('/'))
-            $dir = str($className)
-                ->explode('/')
-                ->tap(function (Collection $data) use (&$className): void {
-                    $className = $data->last();
-                })
-                ->slice(0, -1)
-                ->values()
-                ->prepend('Pages')
-                ->implode('/');
+        if(str($className)->contains('/')) {
+            $dir.= str(dirname($className))
+                ->start('/');
+            $className = class_basename($className);
+        }
 
         $page = $this->getDirectory() . "/$dir/$className.php";
 
