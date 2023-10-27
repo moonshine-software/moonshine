7aaa984dc62fee502da1b0de0c37827eab9a8b3b
LT <leetoplayaz@yandex.ru>
2023-10-27 11:20:15 +0300

    feat: Default config option for import/export

--- config/moonshine.php
+++ config/moonshine.php
@@ -41,6 +41,11 @@ return [
         'profile' => ProfilePage::class
     ],
 
+    'model_resources' => [
+        'default_with_import' => false,
+        'default_with_export' => false,
+    ],
+
     'auth' => [
         'enable' => true,
         'middleware' => Authenticate::class,
