c7e989516d452c2d2305b3bd3bff1697da9c12f5
Alexander Opekunov <iam@opekunov.com>
2023-11-02 17:30:54 +0300

    refactor: switch x-data to alpine component

--- resources/views/components/form/input-extensions/up-down.blade.php
+++ resources/views/components/form/input-extensions/up-down.blade.php
@@ -1,24 +1,20 @@
 @props(['extension'])
-<button @click.prevent="toggleDown()"
-        {{ $attributes->class(['expansion']) }}
-        type="button"
->
-    <span>
-        <x-moonshine::icon
-            icon="heroicons.minus-small"
-            size="4"
-        />
-    </span>
-</button>
+<div class="expansion-custom flex" x-data="numberUpDown">
+    <button @click.prevent="toggleDown()" class="px-2" type="button">
+        <span>
+            <x-moonshine::icon
+                icon="heroicons.minus-small"
+                size="4"
+            />
+        </span>
+    </button>
 
-<button @click.prevent="toggleUp()"
-        {{ $attributes->class(['expansion']) }}
-        type="button"
->
-    <span>
-        <x-moonshine::icon
-            icon="heroicons.plus-small"
-            size="4"
-        />
-    </span>
-</button>
+    <button @click.prevent="toggleUp()"  class="px-2" type="button">
+        <span>
+            <x-moonshine::icon
+                icon="heroicons.plus-small"
+                size="4"
+            />
+        </span>
+    </button>
+</div>
