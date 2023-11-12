73dd8209a90d145eb8f8437718b15cf82d5baba4
lee-to <lee-to@users.noreply.github.com>
2023-11-12 20:23:59 +0000

    [rector] Rector fixes

--- src/Collections/MoonShineRenderElements.php
+++ src/Collections/MoonShineRenderElements.php
@@ -63,7 +63,7 @@ abstract class MoonShineRenderElements extends Collection
 
     public function exceptElements(Closure $except): self
     {
-        return $this->filter(function ($element) use ($except) {
+        return $this->filter(function ($element) use ($except): bool {
             if ($except($element) === true) {
                 return false;
             }
