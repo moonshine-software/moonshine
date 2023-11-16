c279911445d9d051258a6812fdc0fab16950d592
LT <leetoplayaz@yandex.ru>
2023-11-16 18:53:49 +0300

    feat: FlexibleRender component
    New flexible component

---
+++ src/Components/FlexibleRender.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+
+/**
+ * @method static static make(Closure|View|string $content, Closure|array $additionalData = [])
+ */
+final class FlexibleRender extends MoonshineComponent
+{
+    protected string $view = 'moonshine::components.flexible-render';
+
+    public function __construct(
+        protected Closure|View|string $content,
+        protected Closure|array $additionalData = [],
+    )
+    {
+    }
+
+    protected function viewData(): array
+    {
+        if(is_closure($this->content)) {
+            $this->content = value($this->content, $this->additionalData, $this);
+        }
+
+        if($this->content instanceof View) {
+            $this->content = $this->content
+                ->with(value($this->additionalData))
+                ->render();
+        }
+
+        return [
+            'content' => $this->content
+        ];
+    }
+}
