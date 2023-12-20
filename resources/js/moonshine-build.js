ed7788f3f1a86fe94cb3e53db5fa188fc1e7e039
LT <leetoplayaz@yandex.ru>
2023-12-20 18:43:22 +0300

    chore: Upgrade to Vite 5

--- resources/js/moonshine-build.js
+++ resources/js/moonshine-build.js
@@ -1,26 +1,24 @@
-const fs = require('fs/promises')
-const fsSync = require('fs')
+import {promises as fsPromises, readFileSync, writeFileSync} from 'fs'
 
 const moonShineBuildPlugin = () => ({
   name: 'moonshine-build-plugin',
   async closeBundle() {
     try {
-      //prepend "(()=>{" to app.js
-      const data = fsSync.readFileSync('public/assets/app.js')
-      const fd = fsSync.openSync('public/assets/app.js', 'w+')
-      const insert = Buffer.from('(()=>{')
-      fsSync.writeSync(fd, insert, 0, insert.length, 0)
-      fsSync.writeSync(fd, data, 0, data.length, insert.length)
-      fsSync.close(fd, err => {
-        if (err) {
-          throw err
-        }
-      })
+      // prepend "(()=>{" to app.js
+      const filePath = 'public/assets/app.js';
+      const data = readFileSync(filePath);
+      const insert = Buffer.from('(()=>{');
 
-      //append "})()" to app.js
-      await fs.appendFile('public/assets/app.js', '})()')
-    } catch (e) {}
+      // Use writeFileSync without the third argument for encoding
+      writeFileSync(filePath, insert);
+      writeFileSync(filePath, data, { flag: 'a' }); // Use { flag: 'a' } for append mode
+
+      // append "})()" to app.js
+      await fsPromises.appendFile(filePath, '})()');
+    } catch (e) {
+      console.error(e);
+    }
   },
-})
+});
 
-export default moonShineBuildPlugin
+export default moonShineBuildPlugin;
