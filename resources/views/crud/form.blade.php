e98038a5da1d8b57538576964321fdb1d0986048
Alexey <bubnov.alexey@gmail.com>
2024-01-29 23:57:57 +0300

    Remove non DRY

--- resources/views/crud/form.blade.php
+++ resources/views/crud/form.blade.php
@@ -17,14 +17,8 @@
 
 @section('content')
     @if(!$resource->isRelatable())
-        @if(count($metrics))
-            <div class="pb-10">
-                <div class="flex flex-col gap-y-8 gap-x-6 sm:grid sm:grid-cols-12 lg:gap-y-10">
-                    @foreach($metrics as $metric)
-                        {!! $resource->renderComponent($metric, $resource->getModel()) !!}
-                    @endforeach
-                </div>
-            </div>
+        @if(!$resource->isRelatable())
+            @include('moonshine::crud.shared.metrics', compact('metrics'))
         @endif
     @endif
     @fragment('crud-form')
--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -27,17 +27,8 @@
 
     <x-moonshine::grid>
         @if(!$resource->isRelatable())
-            @if(count($metrics))
-                <x-moonshine::column class="pb-10">
-                    <div class="flex flex-col gap-y-8 gap-x-6 sm:grid sm:grid-cols-12 lg:gap-y-10">
-                        @foreach($metrics as $metric)
-                            {!! $resource->renderComponent($metric, $resource->getModel()) !!}
-                        @endforeach
-                    </div>
-                </x-moonshine::column>
-            @endif
+            @include('moonshine::crud.shared.metrics', compact('metrics'))
         @endif
-
         @if($resource->componentsCollection()->indexComponents()->isNotEmpty())
             @foreach($resource->componentsCollection()->indexComponents() as $indexComponent)
                 @if($indexComponent->isSee($resource->getModel()))
