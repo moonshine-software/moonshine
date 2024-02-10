821c0e1024b009f6e20c80f7a5b959728e965ae6
LT <leetoplayaz@yandex.ru>
2024-02-10 21:16:22 +0530

    fix: Issue
    Issue #799

--- src/Http/Requests/Resources/DeleteFormRequest.php
+++ src/Http/Requests/Resources/DeleteFormRequest.php
@@ -4,22 +4,28 @@ declare(strict_types=1);
 
 namespace MoonShine\Http\Requests\Resources;
 
+use MoonShine\Exceptions\ResourceException;
 use MoonShine\Http\Requests\MoonShineFormRequest;
+use Throwable;
 
 final class DeleteFormRequest extends MoonShineFormRequest
 {
+    /**
+     * @throws Throwable
+     * @throws ResourceException
+     */
     public function authorize(): bool
     {
         $this->beforeResourceAuthorization();
 
         if (! in_array(
             'delete',
-            $this->getResource()->getActiveActions(),
+            $this->getResource()?->getActiveActions() ?? [],
             true
         )) {
             return false;
         }
 
-        return $this->getResource()->can('delete');
+        return $this->getResource()?->can('delete') ?? false;
     }
 }
