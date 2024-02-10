f36e4a05758054ad13ed0466465586ab0eeedef6
KUANYSH OSMANOV <developerandroidkz@gmail.com>
2024-02-10 18:48:24 +0500

    fix: return type Model added

--- src/Contracts/MoonShineDataCast.php
+++ src/Contracts/MoonShineDataCast.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace MoonShine\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface MoonShineDataCast
 {
-    public function getClass(): string;
+    public function getClass(): string|Model;
 
     public function hydrate(array $data): mixed;
 
