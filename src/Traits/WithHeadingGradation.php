1e8cea191a31e80e5b13c04fec2bff45fc2cc1c1
devil <i@akost.dev>
2024-02-23 12:52:28 +0500

    feat: Add heading gradation v4

--- src/Traits/WithHeadingGradation.php
+++ src/Traits/WithHeadingGradation.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace MoonShine\Traits;
 
+use InvalidArgumentException;
+
 trait WithHeadingGradation
 {
     protected int $gradation = 3;
@@ -15,6 +17,12 @@ trait WithHeadingGradation
 
     public function h(int $gradation = 3, $asClass = true): static
     {
+        if ($gradation < 1 || $gradation > 6) {
+            throw new InvalidArgumentException(
+                'gradation must be between 1 and 6'
+            );
+        }
+
         $this->gradation = $gradation;
 
         if ($asClass) {
