30596ececbff4ba232efe885800953f25c636fdd
devil <i@akost.dev>
2024-02-23 11:02:17 +0500

    feat: Add heading gradation v3

--- src/Traits/WithHeadingGradation.php
+++ src/Traits/WithHeadingGradation.php
@@ -6,16 +6,16 @@ namespace MoonShine\Traits;
 
 trait WithHeadingGradation
 {
-    protected string $gradation = '3';
+    protected int $gradation = 3;
 
     public function getH(): string
     {
         return "h$this->gradation";
     }
 
-    public function h(string|int $gradation = '3', $asClass = true): static
+    public function h(int $gradation = 3, $asClass = true): static
     {
-        $this->gradation = preg_replace('/[^0-9]/','', (string) $gradation);
+        $this->gradation = $gradation;
 
         if ($asClass) {
             $this->withAttributes(['class' => $this->getH()]);
