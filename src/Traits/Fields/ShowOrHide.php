be32cbbbc9caff3a68c7507bd1d6634bdf3d4b56
Murchenko <rabix346@yandex.ru>
2024-02-25 15:47:15 +0300

    chore: rename hideFromAll -> hideOnAll

--- src/Traits/Fields/ShowOrHide.php
+++ src/Traits/Fields/ShowOrHide.php
@@ -173,9 +173,9 @@ trait ShowOrHide
     /**
      * Hide entity from all pages/forms
      *
-     * You can use Field::make()->hideFromAll->showOnIndex() for show field only on index page
+     * You can use Field::make()->hideOnAll()->showOnIndex() for show field only on index page
      */
-    public function hideFromAll(): static
+    public function hideOnAll(): static
     {
         $this->showOnCreateForm = false;
         $this->showOnUpdateForm = false;
