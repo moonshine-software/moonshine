d7f35abdb5bd1d9d83ae87152a508887baa4165f
lee-to <lee-to@users.noreply.github.com>
2024-03-18 11:01:32 +0000

    [rector] Rector fixes

--- src/MoonShineRequest.php
+++ src/MoonShineRequest.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace MoonShine;
 
 use Illuminate\Http\Request;
-use MoonShine\Contracts\Resources\ResourceContract;
-use MoonShine\Pages\Page;
 use MoonShine\Traits\Request\HasPageRequest;
 use MoonShine\Traits\Request\HasResourceRequest;
 
