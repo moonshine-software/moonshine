511f755f99ccbf045b667760b3070d6687c0e516
levchenko-ivan <ivan97111@yandex.ru>
2024-04-01 12:26:15 +0300

    fix: Fix duplicate html attribute id for Hidden field
    fix: Fix duplicate html attribute id for Hidden field
    
    fix: Fix duplicate html attribute id for Hidden field
    
    fix: Fix duplicate html attribute id for Hidden field

--- resources/views/fields/hidden.blade.php
+++ resources/views/fields/hidden.blade.php
@@ -1,6 +1,6 @@
 <x-moonshine::form.input
     :attributes="$element->attributes()->merge([
-        'id' => $element->id(),
+        'id' => empty($element->id()) ? null : $element->id(),
         'name' => $element->name(),
         'value' => (string) $value
     ])"
