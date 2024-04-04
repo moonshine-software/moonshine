0c491666b6fbc760427b9cd28b85744f07db5a28
Alexander Nikushkin <nikushkin@mail.ru>
2024-04-04 17:57:24 +0500

    chore: background color for login and 404

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -4,7 +4,7 @@
         @include('moonshine::layouts.shared.head')
         @moonShineAssets
     </head>
-    <body class="antialiased">
+    <body class="antialiased !bg-darkblue">
         @yield('content')
     </body>
 </html>
