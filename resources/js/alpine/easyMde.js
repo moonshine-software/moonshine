0198cdbfed3f4d741f2ffa0a57dd603e81c8e5bf
akostdev <i@akost.dev>
2024-05-19 22:39:22 +0500

    feat: simple changes

--- resources/js/alpine/easyMde.js
+++ resources/js/alpine/easyMde.js
@@ -5,45 +5,46 @@ import DOMPurify from 'dompurify'
 
 export default () => ({
   init() {
-    this.$nextTick(() => {
-      new EasyMDE({
-        element: this.$el,
-        previewClass: ['prose', 'dark:prose-invert'],
-        renderingConfig: {
-          sanitizerFunction: (renderedHTML) => {
-            return DOMPurify.sanitize(renderedHTML, {
-              USE_PROFILES: {
-                html: true
-              }
-            })
-          },
+    new EasyMDE({
+      element: this.$el,
+      previewClass: ['prose', 'dark:prose-invert'],
+      forceSync: true,
+      spellChecker: false,
+      status: false,
+      renderingConfig: {
+        sanitizerFunction: (renderedHTML) => {
+          return DOMPurify.sanitize(renderedHTML, {
+            USE_PROFILES: {
+              html: true
+            }
+          })
         },
-        toolbar: [
-          'bold',
-          'italic',
-          'strikethrough',
-          'code',
-          'quote',
-          'horizontal-rule',
-          '|',
-          'heading-1',
-          'heading-2',
-          'heading-3',
-          '|',
-          'table',
-          'unordered-list',
-          'ordered-list',
-          '|',
-          'link',
-          'image',
-          '|',
-          'preview',
-          'side-by-side',
-          'fullscreen',
-          '|',
-          'guide',
-        ]
-      })
+      },
+      toolbar: [
+        'bold',
+        'italic',
+        'strikethrough',
+        'code',
+        'quote',
+        'horizontal-rule',
+        '|',
+        'heading-1',
+        'heading-2',
+        'heading-3',
+        '|',
+        'table',
+        'unordered-list',
+        'ordered-list',
+        '|',
+        'link',
+        'image',
+        '|',
+        'preview',
+        'side-by-side',
+        'fullscreen',
+        '|',
+        'guide',
+      ]
     })
   },
 })
