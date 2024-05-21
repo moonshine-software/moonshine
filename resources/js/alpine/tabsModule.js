2f6fa4be7fa2321617f6a02a52d5acfd1d9e1cd1
lee-to <lee-to@users.noreply.github.com>
2024-05-21 06:50:34 +0000

    Apply prettier changes

--- resources/js/alpine/tabsModule.js
+++ resources/js/alpine/tabsModule.js
@@ -1,31 +1,29 @@
-export default (activeTab = "", isVertical = false) => ({
-    activeTab: activeTab,
-    isVertical: isVertical,
-    activationWidth: 480,
+export default (activeTab = '', isVertical = false) => ({
+  activeTab: activeTab,
+  isVertical: isVertical,
+  activationWidth: 480,
 
-    async init() {
-        await this.$nextTick();
+  async init() {
+    await this.$nextTick()
 
-        if (this.isVertical) {
-            this.activationWidth = this.$el.dataset.tabsVerticalMinWidth ?? 480;
-            this.toggleVerticalClass(true);
-            this.checkWidthElement();
-            window.addEventListener("resize", () => this.checkWidthElement());
-        }
-    },
+    if (this.isVertical) {
+      this.activationWidth = this.$el.dataset.tabsVerticalMinWidth ?? 480
+      this.toggleVerticalClass(true)
+      this.checkWidthElement()
+      window.addEventListener('resize', () => this.checkWidthElement())
+    }
+  },
 
-    toggleVerticalClass(shouldBeVertical) {
-        this.$el.classList[shouldBeVertical ? "add" : "remove"](
-            "tabs-vertical",
-        );
-    },
+  toggleVerticalClass(shouldBeVertical) {
+    this.$el.classList[shouldBeVertical ? 'add' : 'remove']('tabs-vertical')
+  },
 
-    checkWidthElement() {
-        const shouldBeVertical = this.$el.offsetWidth >= this.activationWidth;
-        this.toggleVerticalClass(shouldBeVertical);
-    },
+  checkWidthElement() {
+    const shouldBeVertical = this.$el.offsetWidth >= this.activationWidth
+    this.toggleVerticalClass(shouldBeVertical)
+  },
 
-    clickingTab(tabId) {
-        this.activeTab = tabId ?? this.activeTab;
-    },
-});
+  clickingTab(tabId) {
+    this.activeTab = tabId ?? this.activeTab
+  },
+})
