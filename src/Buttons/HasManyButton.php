ea450f0ea4505db13e1718368372f39c7b51e7f1
LT <leetoplayaz@yandex.ru>
2024-06-03 17:33:46 +0300

    fix: Issue
    Non async modal

--- src/Buttons/HasManyButton.php
+++ src/Buttons/HasManyButton.php
@@ -39,8 +39,6 @@ final class HasManyButton
             ]
         );
 
-        $isAsync = $resource->isAsync() || $field->isAsync();
-
         $authorize = $update
             ? fn (?Model $item): bool => ! is_null($item) && in_array('update', $resource->getActiveActions())
                 && $resource->setItem($item)->can('update')
@@ -56,7 +54,7 @@ final class HasManyButton
             ->inModal(
                 title: fn (): array|string|null => __($update ? 'moonshine::ui.edit' : 'moonshine::ui.create'),
                 content: '',
-                async: $isAsync,
+                async: true,
                 wide: true,
             )
             ->primary()
