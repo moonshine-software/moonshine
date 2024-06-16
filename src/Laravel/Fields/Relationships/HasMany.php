61184168567275170354d8ded85c632738521efe
lee-to <lee-to@users.noreply.github.com>
2024-06-16 18:30:36 +0000

    [rector] Rector fixes

--- src/Laravel/Fields/Relationships/HasMany.php
+++ src/Laravel/Fields/Relationships/HasMany.php
@@ -8,7 +8,6 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Collection;
 use MoonShine\Laravel\Buttons\HasManyButton;
 use MoonShine\Laravel\Collections\Fields;
 use MoonShine\Laravel\Resources\ModelResource;
