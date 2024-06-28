01587fa9d5f6437d4c4b0146e4bba3c23542e003
lee-to <lee-to@users.noreply.github.com>
2024-06-28 08:26:56 +0000

    Apply prettier changes

--- resources/js/Request/Sets.js
+++ resources/js/Request/Sets.js
@@ -25,25 +25,25 @@ export function listComponentRequest(component, pushState = false) {
 
   let componentRequestData = new ComponentRequestData()
   componentRequestData
-  .withBeforeCallback(function(data, t) {
-    const query = url.slice(url.indexOf('?') + 1)
+    .withBeforeCallback(function (data, t) {
+      const query = url.slice(url.indexOf('?') + 1)
 
-    if (pushState) {
-      history.pushState({}, '', query ? '?' + query : location.pathname)
-    }
+      if (pushState) {
+        history.pushState({}, '', query ? '?' + query : location.pathname)
+      }
 
-    document.querySelectorAll('.js-change-query').forEach(function (element) {
-      element.setAttribute('href', element.dataset.originalUrl + (query ? '?' + query : ''))
+      document.querySelectorAll('.js-change-query').forEach(function (element) {
+        element.setAttribute('href', element.dataset.originalUrl + (query ? '?' + query : ''))
+      })
+
+      if (t.$root.dataset.events) {
+        dispatchEvents(t.$root.dataset.events, 'success', t)
+      }
+
+      t.$root.outerHTML = data
+      t.loading = false
     })
-
-    if (t.$root.dataset.events) {
-      dispatchEvents(t.$root.dataset.events, 'success', t)
-    }
-
-    t.$root.outerHTML = data
-    t.loading = false
-  })
-  .withErrorCallback(stopLoading)
+    .withErrorCallback(stopLoading)
 
   request(component, url, 'get', {}, {}, componentRequestData)
 
