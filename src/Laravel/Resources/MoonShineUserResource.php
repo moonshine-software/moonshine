a09e96ad9d006f96ef54d60c94f67f3b9cb0c933
LT <leetoplayaz@yandex.ru>
2024-07-06 17:22:12 +0300

    chore: Merge 2.x

--- src/Laravel/Resources/MoonShineUserResource.php
+++ src/Laravel/Resources/MoonShineUserResource.php
@@ -101,7 +101,7 @@ class MoonShineUserResource extends ModelResource
                     ]),
 
                     Tab::make(__('moonshine::ui.resource.password'), [
-                        Heading::make('Change password'),
+                        Heading::make(__('moonshine::ui.resource.change_password')),
 
                         Password::make(__('moonshine::ui.resource.password'), 'password')
                             ->customAttributes(['autocomplete' => 'new-password'])
