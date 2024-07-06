4d5a099610b99029f897af19946c841bdd4aba10
LT <leetoplayaz@yandex.ru>
2024-07-06 16:40:48 +0300

    fix: Markdown assets

--- src/UI/Fields/Markdown.php
+++ src/UI/Fields/Markdown.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace MoonShine\UI\Fields;
 
+use MoonShine\AssetManager\Css;
+use MoonShine\AssetManager\Js;
+
 final class Markdown extends Textarea
 {
     protected string $view = 'moonshine::fields.markdown';
 
-    protected array $assets = [
-        'vendor/moonshine/libs/easymde/easymde.min.css',
-        'vendor/moonshine/libs/easymde/easymde.min.js',
-        'vendor/moonshine/libs/easymde/purify.min.js',
-    ];
+    public function getAssets(): array
+    {
+        return [
+            Css::make('vendor/moonshine/libs/easymde/easymde.min.css'),
+            Js::make('vendor/moonshine/libs/easymde/easymde.min.js'),
+            Js::make('vendor/moonshine/libs/easymde/purify.min.js'),
+        ];
+    }
 }
