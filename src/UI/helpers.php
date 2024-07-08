ee4560e757e6035337fe5d2b6cc049d837bad15f
LT <leetoplayaz@yandex.ru>
2024-07-08 18:46:06 +0300

    refactor: Remove helpers

--- src/UI/helpers.php
+++ src/UI/helpers.php
@@ -1,12 +1,9 @@
 <?php
 
 declare(strict_types=1);
-use MoonShine\Support\Enums\FormMethod;
+
 use MoonShine\UI\Applies\AppliesRegister;
 use MoonShine\UI\Collections\Fields;
-use MoonShine\UI\Components\ActionButton;
-use MoonShine\UI\Components\FormBuilder;
-use MoonShine\UI\Components\Table\TableBuilder;
 use MoonShine\UI\Contracts\Collections\FieldsCollection;
 
 if (! function_exists('appliesRegister')) {
@@ -26,35 +23,5 @@ if (! function_exists('fieldsCollection')) {
     {
         return moonshine()
             ->getContainer(FieldsCollection::class, null, items: $items) ?? $default::make($items);
-    }
-}
-
-if (! function_exists('form')) {
-    function form(
-        string $action = '',
-        FormMethod $method = FormMethod::POST,
-        Fields|array $fields = [],
-        array $values = []
-    ): FormBuilder {
-        return FormBuilder::make($action, $method, $fields, $values);
-    }
-}
-
-if (! function_exists('table')) {
-    function table(
-        Fields|array $fields = [],
-        iterable $items = [],
-    ): TableBuilder {
-        return TableBuilder::make($fields, $items);
-    }
-}
-
-if (! function_exists('actionBtn')) {
-    function actionBtn(
-        Closure|string $label,
-        Closure|string $url = '',
-        mixed $item = null
-    ): ActionButton {
-        return ActionButton::make($label, $url, $item);
     }
 }
