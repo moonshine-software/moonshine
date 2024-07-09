0d5754c86a3952f4dc67ddb8a71ee9a5fbfe2075
shevelev-anatoliy <tolik.breathless@yandex.ru>
2024-07-09 22:00:20 +0700

    feat: add random hash to export button link

--- src/Buttons/ExportButton.php
+++ src/Buttons/ExportButton.php
@@ -3,6 +3,7 @@
 namespace MoonShine\Buttons;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use MoonShine\ActionButtons\ActionButton;
 use MoonShine\Handlers\ExportHandler;
 use MoonShine\Resources\ModelResource;
@@ -19,7 +20,7 @@ final class ExportButton
 
         return ActionButton::make(
             $export->label(),
-            $url . ($query ? '?' . $query : '')
+            "{$url}?hash=" . Str::random(8) . ($query ? "&{$query}" : '')
         )
             ->primary()
             ->customAttributes(['class' => '_change-query', 'data-original-url' => $url])
