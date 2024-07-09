4e7252b6a89e1b274c9a26a5eb37008d1ba7a373
shevelev-anatoliy <tolik.breathless@yandex.ru>
2024-07-10 06:59:23 +0700

    refactor

--- src/Buttons/ExportButton.php
+++ src/Buttons/ExportButton.php
@@ -19,7 +19,7 @@ final class ExportButton
 
         return ActionButton::make(
             $export->label(),
-            "{$url}?timestamp=" . time() . ($query ? "&{$query}" : '')
+            trim("$url?ts=".time()."&$query", '&')
         )
             ->primary()
             ->customAttributes(['class' => '_change-query', 'data-original-url' => $url])
