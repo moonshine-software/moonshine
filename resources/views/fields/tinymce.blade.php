66f6a689e60dbc8b8b400a1a28bd0385367f44a7
Alexander Nikushkin <nikushkin@mail.ru>
2024-07-15 19:00:20 +0500

    chore: fix style for Select and TinyMce fields when form-invalid

--- resources/views/fields/tinymce.blade.php
+++ resources/views/fields/tinymce.blade.php
@@ -6,6 +6,7 @@
         :attributes="$element->attributes()->merge([
             'name' => $element->name()
         ])->except('x-bind:id')"
+        @class(['form-invalid' => formErrors($errors, $element->getFormName())->has($element->name())])
         ::id="$id('tiny-mce')"
         x-data="tinymce({{ $config }})"
     >{!! $value ?? '' !!}</x-moonshine::form.textarea>
