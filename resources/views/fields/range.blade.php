9eff459c4bc86cbcf77468577de3f77607f0186c
Alexander Nikushkin <nikushkin@mail.ru>
2024-07-18 19:28:30 +0500

    refactoring: validation message for Range field

--- resources/views/fields/range.blade.php
+++ resources/views/fields/range.blade.php
@@ -5,43 +5,43 @@
      }"
     {{ $element->attributes()
         ->only('class')
-        ->merge(['class' => 'form-group form-group-inline items-start']) }}
+        ->merge(['class' => 'form-group form-group-inline']) }}
 
     data-field-block="{{ $element->name() }}"
 >
-    <x-moonshine::form.input-wrapper
-        class="!my-0"
-        :formName="$element->getFormName()"
-        :name="$element->getNameDotFrom()"
-    >
-        <x-moonshine::form.input
-            :attributes="$element->getFromAttributes()->merge([
-                'name' => $element->name($element->fromField),
-            ])"
-            @class([
-                'form-invalid' => formErrors($errors ?? false, $element->getFormName())->has($element->getNameDotFrom())
-            ])
-            x-bind:max="range_to_{{ $element->id() }}"
-            x-model="range_from_{{ $element->id() }}"
-            value="{{ $value[$element->fromField] ?? '' }}"
-        />
-    </x-moonshine::form.input-wrapper>
+    <x-moonshine::form.input
+        :attributes="$element->getFromAttributes()->merge([
+            'name' => $element->name($element->fromField),
+        ])"
+        @class([
+            'form-invalid' => formErrors($errors ?? false, $element->getFormName())->has($element->getNameDotFrom())
+        ])
+        x-bind:max="range_to_{{ $element->id() }}"
+        x-model="range_from_{{ $element->id() }}"
+        value="{{ $value[$element->fromField] ?? '' }}"
+    />
 
-    <x-moonshine::form.input-wrapper
-        class="!my-0"
-        :formName="$element->getFormName()"
-        :name="$element->getNameDotTo()"
-    >
-        <x-moonshine::form.input
-            :attributes="$element->getToAttributes()->merge([
-                'name' => $element->name($element->toField)
-            ])"
-            @class([
-                'form-invalid' => formErrors($errors ?? false, $element->getFormName())->has($element->getNameDotTo())
-            ])
-            x-bind:min="range_from_{{ $element->id() }}"
-            x-model="range_to_{{ $element->id() }}"
-            value="{{ $value[$element->toField] ?? '' }}"
-        />
-    </x-moonshine::form.input-wrapper>
+    <x-moonshine::form.input
+        :attributes="$element->getToAttributes()->merge([
+            'name' => $element->name($element->toField)
+        ])"
+        @class([
+            'form-invalid' => formErrors($errors ?? false, $element->getFormName())->has($element->getNameDotTo())
+        ])
+        x-bind:min="range_from_{{ $element->id() }}"
+        x-model="range_to_{{ $element->id() }}"
+        value="{{ $value[$element->toField] ?? '' }}"
+    />
 </div>
+
+@error($element->getNameDotFrom(), $element->getFormName())
+<x-moonshine::form.input-error>
+    {{ $message }}
+</x-moonshine::form.input-error>
+@enderror
+
+@error($element->getNameDotTo(), $element->getFormName())
+<x-moonshine::form.input-error>
+    {{ $message }}
+</x-moonshine::form.input-error>
+@enderror
