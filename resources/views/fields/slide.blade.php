d169a8f3460d412090751f5a422d05c7d0c0e884
Alexander Nikushkin <nikushkin@mail.ru>
2024-07-18 19:29:38 +0500

    fix: validation message for RangeSlider field

--- resources/views/fields/slide.blade.php
+++ resources/views/fields/slide.blade.php
@@ -1,13 +1,28 @@
 <x-moonshine::form.slide-range
     :uniqueId="$element->id()"
     :attributes="$element->attributes()"
-    :fromAttributes="$element->getFromAttributes()"
-    :toAttributes="$element->getToAttributes()"
+    :fromAttributes="$element->getFromAttributes()->class([
+        'form-invalid' => formErrors(session('errors', false), $element->getFormName())->has($element->getNameDotFrom())
+    ])"
+    :toAttributes="$element->getToAttributes()->class([
+        'form-invalid' => formErrors(session('errors', false), $element->getFormName())->has($element->getNameDotTo())
+    ])"
     :fromValue="$value[$element->fromField] ?? $element->min"
     :toValue="$value[$element->toField] ?? $element->max"
     fromName="{{ $element->name($element->fromField) }}"
     toName="{{ $element->name($element->toField) }}"
-    fromField="{{ $element->column() }}.{{ $element->fromField }}"
-    toField="{{ $element->column() }}.{{ $element->toField }}"
-    @class(['form-invalid' => formErrors($errors, $element->getFormName())->has($element->name())])
+    fromField="{{ $element->getNameDotFrom() }}"
+    toField="{{ $element->getNameDotTo() }}"
 />
+
+@error($element->getNameDotFrom(), $element->getFormName())
+    <x-moonshine::form.input-error>
+        {{ $message }}
+    </x-moonshine::form.input-error>
+@enderror
+
+@error($element->getNameDotTo(), $element->getFormName())
+    <x-moonshine::form.input-error>
+        {{ $message }}
+    </x-moonshine::form.input-error>
+@enderror
