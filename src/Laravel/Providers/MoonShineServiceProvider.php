79c7a6fa0f8e6e2c4770d4ab1b24ebe9956e6b89
lee-to <lee-to@users.noreply.github.com>
2024-07-22 16:22:07 +0000

    Fix styling

--- src/Laravel/Providers/MoonShineServiceProvider.php
+++ src/Laravel/Providers/MoonShineServiceProvider.php
@@ -129,7 +129,7 @@ class MoonShineServiceProvider extends ServiceProvider
     {
         $this->app->singleton(CoreContract::class, MoonShine::class);
 
-        Core::setInstance(static fn() => app(CoreContract::class));
+        Core::setInstance(static fn () => app(CoreContract::class));
 
         $this->app->bind(RouterContract::class, MoonShineRouter::class);
 
