aed8a373532e3a08bbfc38185f83f750e5c22a53
LT <leetoplayaz@yandex.ru>
2024-07-24 18:02:37 +0300

    refactor: Move to Packages
    And refactoring

--- src/Laravel/Layouts/BlankLayout.php
+++ src/Laravel/Layouts/BlankLayout.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace MoonShine\Laravel\Layouts;
 
 use MoonShine\UI\Components\Components;
-use MoonShine\UI\Components\Layout\{Assets, Body, Favicon, Head, Html, LayoutBuilder, Meta}
+use MoonShine\UI\Components\Layout\{Body, Html, LayoutBuilder};
 
 final class BlankLayout extends BaseLayout
 {
