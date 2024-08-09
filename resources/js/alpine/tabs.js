a9e6526df3d06e4dcf71d8599ae5258b4ba5c7a6
Alexander Nikushkin <nikushkin@mail.ru>
2024-08-09 21:14:34 +0500

    fix: toggle vertical style for hidden tabs
    issue #1169

--- resources/js/alpine/tabs.js
+++ resources/js/alpine/tabs.js
@@ -10,6 +10,8 @@ export default (activeTab = '', isVertical = false) => ({
       this.activationVerticalWidth = this.$el.dataset.tabsVerticalMinWidth ?? 480
       this.toggleVerticalClass(true)
       this.checkWidthElement()
+
+      window.addEventListener('active-tab', () => this.checkWidthElement())
       window.addEventListener('resize', () => this.checkWidthElement())
     }
   },
@@ -25,5 +27,9 @@ export default (activeTab = '', isVertical = false) => ({
 
   setActiveTab(tabId) {
     this.activeTab = tabId ?? this.activeTab
+
+    setTimeout(function() {
+      window.dispatchEvent(new Event('active-tab'))
+    });
   },
 })
