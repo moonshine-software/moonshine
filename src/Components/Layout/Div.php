1043a0aa180e60b19fae6db82615ad79c1c7fe4b
LT <leetoplayaz@yandex.ru>
2024-08-29 18:41:24 +0300

    fix: Div component
    Change Component to Decoration

--- src/Components/Layout/Div.php
+++ src/Components/Layout/Div.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace MoonShine\Components\Layout;
 
-class Div extends WithComponents
+use MoonShine\Decorations\Decoration;
+
+class Div extends Decoration
 {
     protected string $view = 'moonshine::components.layout.div';
 }
