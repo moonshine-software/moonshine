9473f4a3013757e50cd70b65b9de822a18cc9140
Alexander Nikushkin <nikushkin@mail.ru>
2024-09-01 11:45:39 +0500

    chore: flex component margin
    issue #1218

--- resources/views/decorations/flex.blade.php
+++ resources/views/decorations/flex.blade.php
@@ -1,7 +1,9 @@
 <div
     {{ $attributes
         ->class([
+            'form-group',
             'sm:flex',
+            'sm:flex-row',
             'gap-4' => !$element->isWithoutSpace(),
             'items-' . $element->getItemsAlign(),
             'justify-' . $element->getJustifyAlign()
