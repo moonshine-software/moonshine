458cbdd43457486758200115d18d369576c85f08
Alexander Nikushkin <nikushkin@mail.ru>
2024-09-04 18:39:06 +0500

    fix: style for inline group form

--- src/Fields/Checkbox.php
+++ src/Fields/Checkbox.php
@@ -44,7 +44,7 @@ class Checkbox extends Field implements
     {
         $this->beforeLabel();
         $this->customWrapperAttributes([
-            'class' => 'form-group-inline',
+            'class' => 'form-group-inline flex-wrap',
         ]);
 
         $this->customAttributes([
