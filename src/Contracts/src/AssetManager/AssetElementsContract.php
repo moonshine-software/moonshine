046f3ae73c64a14789f5d09ddc36dac10ecd7463
lee-to <lee-to@users.noreply.github.com>
2024-09-12 09:32:54 +0000

    [rector] Rector fixes

--- src/Contracts/src/AssetManager/AssetElementsContract.php
+++ src/Contracts/src/AssetManager/AssetElementsContract.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace MoonShine\Contracts\AssetManager;
 
 use Illuminate\Contracts\Support\Htmlable;
-use Traversable;
 
 interface AssetElementsContract extends Htmlable
 {
