812982c64b4542c7fbe8426c9a1be255b204992b
Danil Shutsky <leetoplayaz@yandex.ru>
2025-07-17 11:53:04 +0300

    feat: Abstract endpoints

--- src/Laravel/src/MoonShineEndpoints.php
+++ src/Laravel/src/MoonShineEndpoints.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace MoonShine\Laravel;
 
 use MoonShine\Contracts\Core\CrudResourceContract;
-use MoonShine\Contracts\Core\DependencyInjection\EndpointsContract;
-use MoonShine\Contracts\Core\DependencyInjection\RouterContract;
 use MoonShine\Contracts\Core\PageContract;
 use MoonShine\Contracts\Core\ResourceContract;
+use MoonShine\Core\AbstractEndpoints;
 use MoonShine\Core\Exceptions\EndpointException;
 use MoonShine\Core\Pages\Pages;
 use MoonShine\Support\Enums\PageType;
@@ -17,90 +16,10 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Throwable;
 
 /**
- * @implements EndpointsContract<CrudResourceContract>
+ * @extends AbstractEndpoints<CrudResourceContract>
  */
-final readonly class MoonShineEndpoints implements EndpointsContract
+final readonly class MoonShineEndpoints extends AbstractEndpoints
 {
-    public function __construct(
-        private RouterContract $router
-    ) {
-    }
-
-    /**
-     * @param  array<string, string|int|float|null>  $params
-     */
-    public function method(
-        string $method,
-        ?string $message = null,
-        array $params = [],
-        ?PageContract $page = null,
-        ?ResourceContract $resource = null
-    ): string {
-        return $this->router->to('method', [
-            'method' => $method,
-            'message' => $message,
-            ...$params,
-            ...[
-                'pageUri' => $this->router->getParam('pageUri', $this->router->extractPageUri($page)),
-                'resourceUri' => $this->router->getParam('resourceUri', $this->router->extractResourceUri($resource)),
-            ],
-        ]);
-    }
-
-    public function reactive(
-        ?PageContract $page = null,
-        ?ResourceContract $resource = null,
-        array $extra = []
-    ): string {
-        $key = $extra['key'] ?? $resource?->getItem()?->getKey();
-
-        return $this->router->to('reactive', [
-            'resourceItem' => $key,
-            'pageUri' => $this->router->getParam('pageUri', $this->router->extractPageUri($page)),
-            'resourceUri' => $this->router->getParam('resourceUri', $this->router->extractResourceUri($resource)),
-        ]);
-    }
-
-    /**
-     * @param  array<string, mixed>  $additionally
-     */
-    public function component(
-        string $name,
-        array $additionally = []
-    ): string {
-        return $this->router->to('component', [
-            '_component_name' => $name,
-            '_parentId' => moonshineRequest()->getParentResourceId(),
-            ...$additionally,
-            ...array_filter([
-                'pageUri' => $this->router->extractPageUri(),
-                'resourceUri' => $this->router->extractResourceUri(),
-                'resourceItem' => $this->router->extractResourceItem($additionally['resourceItem'] ?? null),
-            ]),
-        ]);
-    }
-
-    /**
-     * @param array<string, mixed> $extra
-     */
-    public function updateField(
-        ?ResourceContract $resource = null,
-        ?PageContract $page = null,
-        array $extra = [],
-    ): string {
-        $relation = $extra['relation'] ?? null;
-        $resourceItem = $extra['resourceItem'] ?? null;
-        $through = $relation ? 'relation' : 'column';
-
-        return $this->withRelation(
-            "update-field.through-$through",
-            resourceItem: $this->router->extractResourceItem($resourceItem),
-            relation: $relation,
-            resourceUri: $resource ? $resource->getUriKey() : $this->router->extractResourceUri(),
-            pageUri: $page ? $page->getUriKey() : $this->router->extractPageUri()
-        );
-    }
-
     /**
      * @param  class-string<PageContract>|PageContract|null  $page
      * @param  class-string<ResourceContract>|ResourceContract|null  $resource
@@ -188,23 +107,5 @@ final readonly class MoonShineEndpoints implements EndpointsContract
         return route(
             moonshineConfig()->getHomeRoute()
         );
-    }
-
-    public function withRelation(
-        string $action,
-        int|string|null $resourceItem = null,
-        ?string $relation = null,
-        ?string $resourceUri = null,
-        ?string $pageUri = null,
-        ?string $parentField = null,
-    ): string {
-        return $this->router->to($action, [
-            'pageUri' => $pageUri ?? moonshineRequest()->getPageUri(),
-            'resourceUri' => $resourceUri ?? moonshineRequest()->getResourceUri(),
-            'resourceItem' => $resourceItem,
-            '_parent_field' => $parentField,
-            '_relation' => $relation,
-            '_no_items_query' => true,
-        ]);
     }
 }
