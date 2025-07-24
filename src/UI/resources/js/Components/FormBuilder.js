a1fae5454c04eff3905b1420e81da1227842a079
Danil Shutsky <leetoplayaz@yandex.ru>
2025-07-24 14:23:11 +0300

    fix: remove dropzone on form reset

--- src/UI/resources/js/Components/FormBuilder.js
+++ src/UI/resources/js/Components/FormBuilder.js
@@ -322,6 +322,9 @@ export default (name = '', initData = {}, reactive = {}) => ({
     })
 
     this.$el.setAttribute('data-reset', '1')
+
+    this.$el.querySelectorAll('[data-remove-on-form-reset]')
+      .forEach((el) => el.remove())
   },
 
   showWhenChange,
