e23587fb0478de233dafd8b3c9b1133da06fa364
second1996 <vital10012@gmail.com>
2025-10-14 01:48:50 +0300

    feat: Accordion component refactoring

--- src/UI/resources/views/components/collapse.blade.php
+++ src/UI/resources/views/components/collapse.blade.php
@@ -16,32 +16,30 @@
         @endif
     "
 >
-    <div
-        class="accordion-item"
-    >
+    <div class="accordion-item" :class="{ '_is-opened': open }">
+        <button 
+            type="button"
+            class="accordion-btn"
+            :class="{ '_is-active': open }"
+            @click.prevent="toggle()"
+        >
+            <div class="flex gap-2 items-center">
+                {{ $icon ?? '' }}
+                {!! $title !!}
+            </div>
 
-        <h2 class="accordion-header">
-            <button type="button" @click.prevent="toggle()" :class="{ '_is-active': open }"
-                    class="accordion-btn btn"
-                    type="button"
-            >
-                <div class="flex gap-2">
-                    {{ $icon ?? '' }}
-                    {!! $title !!}
-                </div>
-
-                @if($button ?? false)
-                    {!! $button !!}
-                @else
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                    </svg>
-                @endif
-            </button>
-        </h2>
-        <div x-cloak
-             :class="open ? 'block' : 'hidden'"
-             class="accordion-body"
+            @if($button ?? false)
+                {!! $button !!}
+            @else
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+            @endif
+        </button>
+        <div 
+            x-cloak
+            :class="open ? 'block' : 'hidden'"
+            class="accordion-body"
         >
             <div class="accordion-content space-elements">
                 <x-moonshine::components
