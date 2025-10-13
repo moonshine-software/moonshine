eaa342c547e5085b8d368f75e9903cdf703d6151
Danil Shutsky <leetoplayaz@yandex.ru>
2025-10-13 21:48:37 +0300

    fix: color mutator

--- src/UI/vite.config.js
+++ src/UI/vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig(({mode}) => {
         },
       },
     },
-    define: { // Прописать только для DEV версии?
+    define: {
       'process.env.CHOICES_CAN_USE_DOM': true,
     },
   }
