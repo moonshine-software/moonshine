7e99196c663969dd00febcc3c9c7868dd664d6f1
second1996 <vital10012@gmail.com>
2025-10-14 12:50:58 +0300

    fix: Removed unnecessary link native text hover color

--- src/UI/resources/views/components/link-native.blade.php
+++ src/UI/resources/views/components/link-native.blade.php
@@ -3,7 +3,7 @@
     'filled' => false,
     'badge' => false,
 ])
-<a {{ $attributes->class(['inline-flex items-center gap-1 max-w-full hover:text-primary', 'text-primary' => $filled]) }}>
+<a {{ $attributes->class(['inline-flex items-center gap-1 max-w-full', 'text-primary' => $filled]) }}>
     {{ $icon ?? '' }}
     {{ $slot ?? '' }}
     @if($badge !== false)
