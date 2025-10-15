fa751b8c04d35e7c755303214b28cb2265bbf3d8
second1996 <vital10012@gmail.com>
2025-10-16 02:02:58 +0300

    feat: Modal component refactoring

--- src/UI/resources/views/components/modal.blade.php
+++ src/UI/resources/views/components/modal.blade.php
@@ -23,12 +23,12 @@
         >
             <div
                 x-show="open"
-                x-transition:enter="transition ease-out duration-300"
-                x-transition:enter-start="opacity-0 -translate-y-10"
-                x-transition:enter-end="opacity-100 translate-y-0"
+                x-transition:enter="transition ease-out duration-250"
+                x-transition:enter-start="opacity-0 scale-95"
+                x-transition:enter-end="opacity-100 scale-100"
                 x-transition:leave="transition ease-in duration-150"
-                x-transition:leave-start="opacity-100 translate-y-0"
-                x-transition:leave-end="opacity-0 -translate-y-10"
+                x-transition:leave-start="opacity-100 scale-100"
+                x-transition:leave-end="opacity-0 scale-95"
                 aria-modal="true"
                 role="dialog"
                 {{ $attributes->merge(['class' => 'modal']) }}
