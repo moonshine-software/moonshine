a57db31501e32464826705df29c1f8743825af2f
second1996 <vital10012@gmail.com>
2025-10-30 11:40:01 +0200

    feat: Sidebar collapse button + icons (migrated to layout navigation slot)

--- src/UI/resources/views/components/layout/header.blade.php
+++ src/UI/resources/views/components/layout/header.blade.php
@@ -2,6 +2,21 @@
     'components' => [],
 ])
 <div {{ $attributes->merge(['class' => 'layout-navigation']) }}>
+    <button
+        type="button"
+        @click.prevent="minimizedMenu = ! minimizedMenu"
+        class="collapse-sidebar-btn btn btn-square max-lg:hidden"
+        title="{{ $translates['collapse_menu'] ?? 'Collapse sidebar' }}"
+    >
+        <div class="menu-icon" x-show="!minimizedMenu">
+            <x-moonshine::icon icon="sidebar-close" />
+        </div>
+
+        <div class="menu-icon" x-cloak x-show="minimizedMenu">
+            <x-moonshine::icon icon="sidebar-open" />
+        </div>
+    </button>
+
     <x-moonshine::components
         :components="$components"
     />
