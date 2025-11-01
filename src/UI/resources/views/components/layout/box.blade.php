bee0e98bcb46b385bb6396a1fac4e61c8973d290
second1996 <vital10012@gmail.com>
2025-11-01 17:52:21 +0200

    fix: Box dark variant

--- src/UI/resources/views/components/layout/box.blade.php
+++ src/UI/resources/views/components/layout/box.blade.php
@@ -4,7 +4,7 @@
     'dark' => false,
     'icon' => null,
 ])
-<div {{ $attributes->class(['box space-elements', 'box-dark dark' => $dark]) }}>
+<div {{ $attributes->class(['box space-elements', 'box--dark' => $dark]) }}>
     @if($label || $icon->isNotEmpty()) <h2 class="box-title">{{ $icon ?? '' }}{{ $label ?? '' }}</h2> @endif
 
     <x-moonshine::components
