e24d95c04fd09d683cfd9c9bed803381eeced2e4
lee-to <thecutcode@gmail.com>
2025-11-03 12:06:52 +0300

    feat: ColorManager 2.0

--- src/ColorManager/src/ColorManager.php
+++ src/ColorManager/src/ColorManager.php
@@ -12,24 +12,10 @@ use MoonShine\ColorManager\Palettes\DefaultPalette;
 use MoonShine\Contracts\ColorManager\ColorManagerContract;
 use MoonShine\Contracts\ColorManager\PaletteContract;
 
-/**
- * @method self primary(string $value, ?int $shade = null, bool $dark = false)
- * @method self secondary(string $value, ?int $shade = null, bool $dark = false)
- * @method self body(string $value, ?int $shade = null, bool $dark = false)
- * @method self base(string $value, int|string|null $shade = null, bool $dark = false)
- * @method self successBg(string $value, ?int $shade = null, bool $dark = false)
- * @method self successText(string $value, ?int $shade = null, bool $dark = false)
- * @method self warningBg(string $value, ?int $shade = null, bool $dark = false)
- * @method self warningText(string $value, ?int $shade = null, bool $dark = false)
- * @method self error(string $value, ?int $shade = null, bool $dark = false)
- * @method self errorBg(string $value, ?int $shade = null, bool $dark = false)
- * @method self errorText(string $value, ?int $shade = null, bool $dark = false)
- * @method self infoBg(string $value, ?int $shade = null, bool $dark = false)
- * @method self infoText(string $value, ?int $shade = null, bool $dark = false)
- */
 final class ColorManager implements ColorManagerContract
 {
     use Conditionable;
+    use ColorShortcuts;
 
     /**
      * @var array<string, string|array<string|int, string>>
@@ -57,267 +43,30 @@ final class ColorManager implements ColorManagerContract
         return $this;
     }
 
-    public function background(string $value): static
+    /**
+     * @param  string|array<string|int, string>  $value
+     *
+     */
+    public function set(string $name, string|array $value, bool $dark = false, bool $everything = false): static
     {
-        return $this
-            ->set('ms-layout-body-bg-color', $value);
-    }
+        if($everything) {
+            return $this->setEverything($name, $value);
+        }
 
-    public function pageBackground(string $value): static
-    {
-        return $this
-            ->set('ms-layout-page-bg-color', $value);
-    }
+        /** @phpstan-ignore-next-line */
+        data_set($this->{$dark ? 'darkColors' : 'colors'}, $name, $value);
 
-    public function borders(string $value): static
-    {
-        return $this
-            ->set('base.stroke', $value);
-    }
-
-    public function dividers(string $value): static
-    {
-        return $this
-            ->set('ms-hr-divider-border-color', $value)
-            ->set('ms-divider-line-bg-color', $value)
-            ->set('ms-divider-color', $value);
-    }
-
-    // Buttons
-    public function button(string $value): static
-    {
-        return $this
-            ->set('ms-btn-bg-color', $value);
-    }
-
-    public function buttonText(string $value): static
-    {
-        return $this
-            ->set('ms-btn-color', $value);
-    }
-
-    public function buttonHover(string $value): static
-    {
-        return $this
-            ->set('ms-btn-hover-bg-color', $value);
-    }
-
-    public function buttonHoverText(string $value): static
-    {
-        return $this
-            ->set('ms-btn-hover-color', $value);
-    }
-
-    // Alerts
-    public function alert(string $value): static
-    {
-        return $this
-            ->set('ms-alert-bg-color', $value);
-    }
-
-    public function alertText(string $value): static
-    {
-        return $this
-            ->set('ms-alert-color', $value);
-    }
-
-    // Badges
-    public function badge(string $value): static
-    {
-        return $this
-            ->set('ms-badge-bg-color', $value);
-    }
-
-    public function badgeText(string $value): static
-    {
-        return $this
-            ->set('ms-badge-color', $value);
-    }
-
-    // Collapse
-    public function collapse(string $value): static
-    {
-        return $this
-            ->set('ms-accordion-item-bg-color', $value);
-    }
-
-    public function collapseText(string $value): static
-    {
-        return $this
-            ->set('ms-accordion-btn-color', $value)
-            ->set('ms-accordion-item-color', $value);
-    }
-
-    public function collapseOpen(string $value): static
-    {
-        return $this
-            ->set('ms-accordion-item-opened-bg-color', $value);
-    }
-
-    public function collapseOpenText(string $value): static
-    {
-        return $this
-            ->set('ms-accordion-btn-active-color', $value)
-            ->set('ms-accordion-item-opened-color', $value);
-    }
-
-    // Popovers
-    public function popover(string $value): static
-    {
-        return $this
-            ->set('ms-popover-border-color', $value)
-            ->set('ms-popover-bg-color', $value);
-    }
-
-    public function popoverText(string $value): static
-    {
-        return $this
-            ->set('ms-popover-color', $value);
-    }
-
-    // Progress bars
-    public function progress(string $value): static
-    {
-        return $this
-            ->set('ms-progress-bar-bg-color', $value)
-            ->set('ms-radial-progress-track-color', $value);
-    }
-
-    public function progressText(string $value): static
-    {
-        return $this
-            ->set('ms-progress-bar-color', $value)
-            ->set('ms-radial-progress-color', $value);
-    }
-
-    // Modal
-    public function modal(string $value): static
-    {
-        return $this
-            ->set('ms-modal-content-bg-color', $value);
-    }
-
-    public function modalText(string $value): static
-    {
-        return $this
-            ->set('ms-modal-content-color', $value);
-    }
-
-    // Offcanvas
-    public function offcanvas(string $value): static
-    {
-        return $this
-            ->set('ms-offcanvas-bg-color', $value);
-    }
-
-    public function offcanvasText(string $value): static
-    {
-        return $this
-            ->set('ms-offcanvas-color', $value);
-    }
-
-    // Box
-    public function box(string $value): static
-    {
-        return $this
-            ->set('ms-box-bg-color', $value);
-    }
-
-    public function boxText(string $value): static
-    {
-        return $this
-            ->set('ms-box-color', $value);
-    }
-
-    public function boxDark(string $value): static
-    {
-        return $this
-            ->set('ms-box-dark-bg-color', $value);
-    }
-
-    public function boxDarkText(string $value): static
-    {
-        return $this
-            ->set('ms-box-dark-color', $value);
-    }
-
-    // Cards
-    public function card(string $value): static
-    {
-        return $this
-            ->set('ms-card-bg-color', $value);
-    }
-
-    public function cardText(string $value): static
-    {
-        return $this
-            ->set('ms-card-color', $value);
-    }
-
-    // Forms
-    public function formDefault(string $value): static
-    {
-        return $this
-            ->set('ms-form-default-bg-color', $value);
-    }
-
-    public function formDefaultText(string $value): static
-    {
-        return $this
-            ->set('ms-form-default-color', $value);
-    }
-
-    public function formFocus(string $value): static
-    {
-        return $this
-            ->set('ms-form-focus-border-color', $value)
-            ->set('ms-form-focus-ring-color', $value);
-    }
-
-    public function formDisabled(string $value): static
-    {
-        return $this
-            ->set('ms-form-disabled-bg-color', $value);
-    }
-
-    public function formDisabledText(string $value): static
-    {
-        return $this
-            ->set('ms-form-disabled-color', $value);
-    }
-
-    public function formReadOnly(string $value): static
-    {
-        return $this
-            ->set('ms-form-readonly-bg-color', $value);
-    }
-
-    public function formReadOnlyText(string $value): static
-    {
-        return $this
-            ->set('ms-form-readonly-color', $value);
-    }
-
-    public function formExpansion(string $value): static
-    {
-        return $this
-            ->set('ms-form-expansion-bg-color', $value);
-    }
-
-    public function formExpansionText(string $value): static
-    {
-        return $this
-            ->set('ms-form-expansion-color', $value);
+        return $this;
     }
 
     /**
      * @param  string|array<string|int, string>  $value
      *
      */
-    public function set(string $name, string|array $value, bool $dark = false): static
+    public function setEverything(string $name, string|array $value): static
     {
-        /** @phpstan-ignore-next-line */
-        data_set($this->{$dark ? 'darkColors' : 'colors'}, $name, $value);
+        $this->set($name, $value);
+        $this->set($name, $value, dark: true);
 
         return $this;
     }
@@ -326,10 +75,10 @@ final class ColorManager implements ColorManagerContract
      * @api
      * @param array<string, string|array<string|int, string>> $colors
      */
-    public function bulkAssign(array $colors, bool $dark = false): static
+    public function bulkAssign(array $colors, bool $dark = false, bool $everything = false): static
     {
         foreach ($colors as $name => $color) {
-            $this->set($name, $color, $dark);
+            $this->set($name, $color, $dark, $everything);
         }
 
         return $this;
---
+++ src/ColorManager/src/ColorShortcuts.php
@@ -0,0 +1,464 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\ColorManager;
+
+trait ColorShortcuts
+{
+    public function text(string $value, bool $dark = false, bool $everything = false): static
+    {
+        return $this
+            ->set('base.text', $value, dark: $dark, everything: $everything);
+    }
+
+    public function background(string $bg, ?string $pageBg = null, bool $dark = false, bool $everything = false): static
+    {
+        if($pageBg !== null) {
+            $this->pageBackground($pageBg, dark: $dark, everything: $everything);
+        }
+
+        return $this
+            ->set('ms-layout-body-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function pageBackground(string $bg, bool $dark = false, bool $everything = false): static
+    {
+        return $this
+            ->set('ms-layout-page-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function borders(string $value, bool $dark = false, bool $everything = false): static
+    {
+        return $this
+            ->set('base.stroke', $value, dark: $dark, everything: $everything);
+    }
+
+    public function dropzone(
+        string $bg,
+        ?string $text = null,
+        ?string $icon = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($icon !== null) {
+            $this->set('ms-dropzone-icon-color', $icon, dark: $dark, everything: $everything);
+        }
+
+        if($text !== null) {
+            $this->set('ms-dropzone-file-color', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this
+            ->set('ms-dropzone-file-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function dividers(string $value, bool $dark = false, bool $everything = false): static
+    {
+        return $this
+            ->bulkAssign([
+                'ms-hr-divider-border-color' => $value,
+                'ms-divider-line-bg-color' => $value,
+                'ms-divider-color' => $value,
+            ], dark: $dark, everything: $everything);
+    }
+
+    /** Buttons */
+
+    public function primary(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('primary-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('primary', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function secondary(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('secondary-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('secondary', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** error, success, info, warning colors with transparency */
+
+    public function error(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('error-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('error-bg', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function success(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('success-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('success-bg', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function warning(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('warning-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('warning-bg', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function info(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('info-text', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('info-bg', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function button(
+        string $bg,
+        ?string $text = null,
+        ?string $hoverBg = null,
+        string $hoverText = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this->set('ms-btn-color', $text, dark: $dark, everything: $everything);
+        }
+
+        if($hoverBg !== null) {
+            $this->set('ms-btn-hover-bg-color', $hoverBg, dark: $dark, everything: $everything);
+        }
+
+        if($hoverText !== null) {
+            $this->set('ms-btn-hover-color', $hoverText, dark: $dark, everything: $everything);
+        }
+
+        return $this->set('ms-btn-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Menu */
+
+    public function menu(
+        string $active,
+        ?string $text = null,
+        ?string $hoverBg = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-menu-item-active-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($hoverBg !== null) {
+            $this
+                ->set('ms-menu-item-hover-bg-color', $hoverBg, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-menu-item-active-bg-color', $active, dark: $dark, everything: $everything)
+            ;
+    }
+
+    /** Dropdown */
+
+    public function dropdown(
+        string $bg,
+        ?string $text = null,
+        ?string $headerBg = null,
+        ?string $footerBg = null,
+        ?string $hoverBg = null,
+        ?string $hoverText = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-dropdown-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($footerBg !== null) {
+            $this
+                ->set('ms-dropdown-footer-bg-color', $footerBg, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($headerBg !== null) {
+            $this
+                ->set('ms-dropdown-heading-bg-color', $headerBg, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($hoverBg !== null) {
+            $this
+                ->set('ms-dropdown-menu-item-hover-bg-color', $hoverBg, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($hoverText !== null) {
+            $this
+                ->set('ms-dropdown-menu-item-hover-color', $hoverText, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-dropdown-bg-color', $bg, dark: $dark, everything: $everything)
+            ;
+    }
+
+    /** Alerts */
+
+    public function alert(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('ms-alert-color', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this
+            ->set('ms-alert-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Badges (has transparency) */
+
+    public function badge(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this->set('ms-badge-color', $text, dark: $dark, everything: $everything);
+        }
+
+        return $this
+            ->set('ms-badge-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Table */
+
+    public function table(
+        string $bg,
+        ?string $headBg = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($headBg !== null) {
+            $this
+                ->set('ms-table-thead-bg-color', $headBg, dark: $dark, everything: $everything);
+        }
+
+        return $this
+            ->set('ms-table-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Collapses */
+
+    public function collapse(
+        string $bg,
+        ?string $text = null,
+        ?string $bgOpen = null,
+        ?string $textOpen = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-accordion-btn-color', $text, dark: $dark, everything: $everything)
+                ->set('ms-accordion-item-color', $text, dark: $dark, everything: $everything)
+                ->set('ms-accordion-btn-active-color', $textOpen ?? $text, dark: $dark, everything: $everything)
+                ->set('ms-accordion-item-opened-color', $textOpen ?? $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-accordion-item-bg-color', $bg, dark: $dark, everything: $everything)
+            ->set('ms-accordion-item-opened-bg-color', $bgOpen ?? $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Popovers */
+
+    public function popover(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-popover-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-popover-border-color', $bg, dark: $dark, everything: $everything)
+            ->set('ms-popover-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Progress bars */
+
+    public function progress(
+        string $bg,
+        string $barBg,
+        ?string $text = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-progress-bar-color', $text, dark: $dark, everything: $everything)
+                ->set('ms-radial-progress-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-progress-bg-color', $bg, dark: $dark, everything: $everything)
+            ->set('ms-progress-bar-bg-color', $barBg, dark: $dark, everything: $everything)
+            ->set('ms-radial-progress-track-color', $barBg, dark: $dark, everything: $everything)
+        ;
+    }
+
+    /** Modals/OffCanvases */
+
+    public function modal(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-modal-content-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-modal-content-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function offCanvas(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-offcanvas-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-offcanvas-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Boxes */
+
+    public function box(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-box-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-box-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    public function boxDark(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-box-dark-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-box-dark-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Cards */
+
+    public function card(string $bg, ?string $text = null, bool $dark = false, bool $everything = false): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-card-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-card-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+
+    /** Forms */
+
+    public function form(
+        string $bg,
+        ?string $text = null,
+        ?string $focus = null,
+        ?string $disabled = null,
+        ?string $disabledText = null,
+        ?string $readonly = null,
+        ?string $readonlyText = null,
+        ?string $expansion = null,
+        ?string $expansionText = null,
+        bool $dark = false,
+        bool $everything = false
+    ): static
+    {
+        if($text !== null) {
+            $this
+                ->set('ms-form-default-color', $text, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($focus !== null) {
+            $this
+                ->set('ms-form-focus-border-color', $focus, dark: $dark, everything: $everything)
+                ->set('ms-form-focus-ring-color', $focus, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($disabled !== null) {
+            $this
+                ->set('ms-form-disabled-bg-color', $disabled, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($disabledText !== null) {
+            $this
+                ->set('ms-form-disabled-color', $disabledText, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($readonly !== null) {
+            $this
+                ->set('ms-form-readonly-bg-color', $readonly, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($readonlyText !== null) {
+            $this
+                ->set('ms-form-readonly-color', $readonlyText, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($expansion !== null) {
+            $this
+                ->set('ms-form-expansion-bg-color', $expansion, dark: $dark, everything: $everything)
+            ;
+        }
+
+        if($expansionText !== null) {
+            $this
+                ->set('ms-form-expansion-color', $readonlyText, dark: $dark, everything: $everything)
+            ;
+        }
+
+        return $this
+            ->set('ms-form-default-bg-color', $bg, dark: $dark, everything: $everything);
+    }
+}
