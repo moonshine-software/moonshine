47c187efe75a0d685deda5fef602915b60876c35
lee-to <thecutcode@gmail.com>
2025-11-13 10:43:00 +0300

    fix: Issue
    Issue #1857

--- src/UI/resources/js/Request/Core.js
+++ src/UI/resources/js/Request/Core.js
@@ -80,10 +80,6 @@ export default async function request(
         fields_values: data.fields_values,
       })
 
-      if (data.redirect) {
-        window.location.assign(data.redirect)
-      }
-
       if (isAttachment) {
         downloadFile(fileName, data)
       }
@@ -103,6 +99,12 @@ export default async function request(
       if (componentRequestData.hasAfterResponse()) {
         const afterResponseCallback = componentRequestData.afterResponse(data, type, t)
         afterResponse(afterResponseCallback, data, type, t)
+      }
+
+      if (data.redirect) {
+        await t.$nextTick(() => {
+          window.location.assign(data.redirect)
+        })
       }
     })
   } catch (errorResponse) {
