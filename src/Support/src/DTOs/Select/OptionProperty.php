ddba6329f6ea39c6ff4e6188a7f512f2d0063ef2
lee-to <thecutcode@gmail.com>
2025-11-18 12:30:03 +0300

    fix: tests

--- src/Support/src/DTOs/Select/OptionProperty.php
+++ src/Support/src/DTOs/Select/OptionProperty.php
@@ -16,7 +16,7 @@ final readonly class OptionProperty implements Arrayable
     ) {
     }
 
-    public function getImage(): null|string|OptionImage
+    public function getImage(): null|OptionImage
     {
         if(\is_string($this->image)){
             return new OptionImage(
