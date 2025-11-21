3ebe3cc80ab886acfd9a3ba195b3b30adc2e28f8
lee-to <thecutcode@gmail.com>
2025-11-21 20:06:30 +0300

    feat: BottomBar

---
+++ src/UI/src/Components/Layout/BottomBar.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\UI\Components\Layout;
+
+use MoonShine\UI\Components\AbstractWithComponents;
+
+/**
+ * @method static static make(iterable $components = [])
+ */
+class BottomBar extends AbstractWithComponents
+{
+    protected string $view = 'moonshine::components.layout.bottom-bar';
+
+    protected array $translates = [
+        'back' => 'moonshine::ui.back',
+    ];
+
+    public function __construct(
+        iterable $components = [],
+    ) {
+        parent::__construct($components);
+    }
+}
