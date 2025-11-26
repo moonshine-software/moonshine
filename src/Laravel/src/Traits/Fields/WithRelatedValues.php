cbf3fa4bd9f8848207eda6e99fd44a82e3d277a2
lee-to <thecutcode@gmail.com>
2025-11-26 12:44:58 +0300

    fix: Select with async search
    Issue #1883

--- src/Laravel/src/Traits/Fields/WithRelatedValues.php
+++ src/Laravel/src/Traits/Fields/WithRelatedValues.php
@@ -79,8 +79,11 @@ trait WithRelatedValues
     {
         $formatted = ! \is_null($this->getFormattedValueCallback());
 
-        $values = $this->memoizeValues ?? $this->resolveValuesQuery()->get();
-        $this->memoizeValues = $values;
+        $values = $this->memoizeValues ?? ($this->isAsyncSearch() && !$this->isToOne() ? $this->toValue() : $this->resolveValuesQuery()->get());
+
+        if($values === null || $values instanceof Collection) {
+            $this->memoizeValues = $values;
+        }
 
         $getValue = fn (Model $item) => $formatted ? value(
             $this->getFormattedValueCallback(),
