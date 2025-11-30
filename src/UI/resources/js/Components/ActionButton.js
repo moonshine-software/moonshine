8f400d9e7427056eb3b12bc1dece4161a414793c
wispoz <wispoz@inbox.ru>
2025-11-30 08:57:22 +0300

    make more pretty

--- src/UI/resources/js/Components/ActionButton.js
+++ src/UI/resources/js/Components/ActionButton.js
@@ -30,11 +30,11 @@ export default () => ({
         ? '<div class="spinner spinner--primary spinner-sm"></div>' + btnText
         : btnText
     })
-    let modal = null;
+
     if (this.$el.dataset.hotKeys) {
       const hotkeys = this.$el.dataset.hotKeys.split(',').map(s => s.trim())
-
-      if ((modal = this.$el.closest('.modal'))) {
+      let modal = this.$el.closest('.modal');
+      if (modal) {
         this._modalHotkey(modal, hotkeys)
       } else {
         document.addEventListener('keydown', event => this._hotKey(event, hotkeys))
