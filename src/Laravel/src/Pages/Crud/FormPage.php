9b4c9d9b9dbfa156c0a7b8438753bfd8f2ed6ca3
lee-to <thecutcode@gmail.com>
2025-12-04 17:09:55 +0300

    fix: Issue
    Issue #1903

--- src/Laravel/src/Pages/Crud/FormPage.php
+++ src/Laravel/src/Pages/Crud/FormPage.php
@@ -84,7 +84,7 @@ class FormPage extends CrudFormPage
                         $item,
                         $field->getResource()?->getCaster(),
                     ),
-                ]))->name($field->getRelationName());
+                ]))->canSee(static fn(): bool => $field->isSee())->name($field->getRelationName());
 
             if ($field instanceof HasTabModeContract && $field->isTabMode()) {
                 $tabs[] = Tab::make($field->getLabel(), [
