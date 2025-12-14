d515a2176b70291aaf50ae2702cf0ca55618e202
lee-to <thecutcode@gmail.com>
2025-12-14 16:47:59 +0300

    fix: Issue
    Issue #1908

--- src/Laravel/src/Http/Requests/LoginFormRequest.php
+++ src/Laravel/src/Http/Requests/LoginFormRequest.php
@@ -37,8 +37,8 @@ class LoginFormRequest extends MoonShineFormRequest
     private function getCredentials(): array
     {
         return [
-            moonshineConfig()->getUserField('username', 'email') => request()->getScalar('username'),
-            moonshineConfig()->getUserField('password') => request()->getScalar('password'),
+            moonshineConfig()->getUserField('username', 'email') => $this->input('username'),
+            moonshineConfig()->getUserField('password') => $this->input('password'),
         ];
     }
 
