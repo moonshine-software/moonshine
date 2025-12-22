fc329679801c66605bb08dd7fb1c854b46c3e669
lee-to <thecutcode@gmail.com>
2025-12-22 11:53:18 +0300

    fix: remove Arr from grid

--- src/UI/resources/views/components/layout/grid.blade.php
+++ src/UI/resources/views/components/layout/grid.blade.php
@@ -4,10 +4,7 @@
 ])
 
 <div {{ $attributes->merge([
-    'class' => Arr::toCssClasses([
-        'grid grid-cols-12',
-        "gap-$gap",
-    ]),
+    'class' => "grid grid-cols-12 gap-$gap",
 ]) }}>
     <x-moonshine::components
         :components="$components"
