4bf83c85fce21a48a97eaf2e2e60d67af0ba5622
madzee <32099597+MadZee-M@users.noreply.github.com>
2025-12-30 08:12:07 +0300

    Fix: Select reset

--- src/UI/resources/js/Components/Select.ts
+++ src/UI/resources/js/Components/Select.ts
@@ -83,7 +83,9 @@ export default (asyncUrl = '', settings: UserSettings = {}, plugins: TPluginHash
         }
 
         const _this = this
-        const commonPlugins: Record<string, Record<string, any>> = {}
+        const commonPlugins: Record<string, Record<string, any>> = {
+            change_listener: {}
+        }
 
         this.isMultiple = this.$el.hasAttribute('multiple') || settings.mode === 'multi'
         this.placeholder = this.$el.getAttribute('placeholder')
@@ -234,6 +236,15 @@ export default (asyncUrl = '', settings: UserSettings = {}, plugins: TPluginHash
                     },
                     false
                 )
+            }
+
+            // Fix for form reset event
+            if (this.$el.form) {
+                this.$el.form.addEventListener('reset', () => {
+                    setTimeout(() => {
+                        this.selectInstance.sync()
+                    })
+                })
             }
         })
     },
