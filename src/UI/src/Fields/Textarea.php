8ea29cffbd89f1073ce56abacd8275131937c3f8
lee-to <thecutcode@gmail.com>
2026-02-04 12:28:40 +0300

    feat: Pretty limit

--- src/UI/src/Fields/Textarea.php
+++ src/UI/src/Fields/Textarea.php
@@ -11,12 +11,14 @@ use MoonShine\UI\Contracts\HasDefaultValueContract;
 use MoonShine\UI\Traits\Fields\HasPlaceholder;
 use MoonShine\UI\Traits\Fields\WithDefaultValue;
 use MoonShine\UI\Traits\Fields\WithEscapedValue;
+use MoonShine\UI\Traits\Fields\WithPrettyLimit;
 
 class Textarea extends Field implements HasDefaultValueContract, CanBeString
 {
     use HasPlaceholder;
     use WithDefaultValue;
     use WithEscapedValue;
+    use WithPrettyLimit;
 
     protected string $view = 'moonshine::fields.textarea';
 
